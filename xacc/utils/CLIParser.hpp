f39ef0a1ad9abf70be91daeadc68f0f87713e4c6
Alex McCaskey <mccaskeyaj@ornl.gov>
2017-06-22 16:28:03 +0000

    Adding list-compiler/accelerator command line args

--- xacc/utils/CLIParser.hpp
+++ xacc/utils/CLIParser.hpp
@@ -88,12 +88,14 @@ public:
 		// to this with all OptionsProviders
 		auto compilerOptions = std::make_shared<options_description>(
 				"XACC Options");
-		compilerOptions->add_options()("help", "Help Message")("compiler",
-				value<std::string>()->default_value("scaffold"),
-				"Indicate the compiler to be used.")("writeIR",
-				value<std::string>(), "Persist generated IR to provided file name.")(
-				"loadCompiler", value<std::string>(), "Load a XACC plugin")
-				("loadAccelerator", value<std::string>(), "Load an XACC Accelerator");
+		compilerOptions->add_options()
+				("help", "Help Message")
+				("compiler",value<std::string>()->default_value("scaffold"),"Indicate the compiler to be used.")
+				("persist-ir",value<std::string>(), "Persist generated IR to provided file name.")
+				("load-compiler", value<std::string>(), "Load a XACC plugin")
+				("load-accelerator", value<std::string>(), "Load an XACC Accelerator")
+				("list-compilers", "List all available XACC Compilers")
+				("list-accelerators", "List all available XACC Accelerators");
 
 		// Load all default Compilers and Accelerators,
 		// ie those in XACC INSTALL DIR/lib
@@ -156,17 +158,22 @@ public:
 		}
 
 		// Parse the command line options
-		variables_map compileParameters;
-		store(parse_command_line(argc, argv, *compilerOptions.get()), compileParameters);
-		if (compileParameters.count("help")) {
+		variables_map clArgs;
+		store(parse_command_line(argc, argv, *compilerOptions.get()), clArgs);
+		if (clArgs.count("help")) {
 			std::cout << *compilerOptions.get() << "\n";
+			XACCInfo(
+					"\n[xacc] XACC Finalizing\n[xacc::compiler] Cleaning up Compiler Registry."
+							"\n[xacc::accelerator] Cleaning up Accelerator Registry.");
+			xacc::CompilerRegistry::instance()->destroy();
+			xacc::AcceleratorRegistry::instance()->destroy();
 			exit(0);
 		}
 
 		// If the user provides a path to a compiler plugin,
 		// then load it
-		if (compileParameters.count("loadCompiler")) {
-			auto loadPath = compileParameters["loadCompiler"].as<std::string>();
+		if (clArgs.count("load-compiler")) {
+			auto loadPath = clArgs["load-compiler"].as<std::string>();
 			boost::filesystem::path p(loadPath);
 			namespace dll = boost::dll;
 			dll::shared_library lib(p, dll::load_mode::append_decorations);
@@ -176,13 +183,13 @@ public:
 						"registerCompiler", dll::load_mode::append_decorations);
 				regFunc();
 			}
-			compileParameters.erase("loadCompiler");
+			clArgs.erase("load-compiler");
 		}
 
 		// If the user provides a path to a accelerator plugin,
 		// then load it
-		if (compileParameters.count("loadAccelerator")) {
-			auto loadPath = compileParameters["loadAccelerator"].as<std::string>();
+		if (clArgs.count("load-accelerator")) {
+			auto loadPath = clArgs["load-accelerator"].as<std::string>();
 			boost::filesystem::path p(loadPath);
 			namespace dll = boost::dll;
 			dll::shared_library lib(p, dll::load_mode::append_decorations);
@@ -192,11 +199,39 @@ public:
 						"registerAccelerator", dll::load_mode::append_decorations);
 				regFunc();
 			}
-			compileParameters.erase("loadAccelerator");
+			clArgs.erase("load-accelerator");
+		}
+
+		bool listTypes = false;
+		if (clArgs.count("list-compilers")) {
+			auto ids = CompilerRegistry::instance()->getRegisteredIds();
+			XACCInfo("Available XACC Compilers:");
+			for (auto i : ids) {
+				XACCInfo("\t" + i);
+			}
+			listTypes = true;
+		}
+
+		if (clArgs.count("list-accelerators")) {
+			auto ids = AcceleratorRegistry::instance()->getRegisteredIds();
+			XACCInfo("Available XACC Accelerators:");
+			for (auto i : ids) {
+				XACCInfo("\t" + i);
+			}
+			listTypes = true;
+		}
+
+		if (listTypes) {
+			XACCInfo(
+					"\n[xacc] XACC Finalizing\n[xacc::compiler] Cleaning up Compiler Registry."
+							"\n[xacc::accelerator] Cleaning up Accelerator Registry.");
+			xacc::CompilerRegistry::instance()->destroy();
+			xacc::AcceleratorRegistry::instance()->destroy();
+			exit(0);
 		}
 
 		// Add all other string options to the global runtime option
-		for (auto it = compileParameters.begin(); it != compileParameters.end();
+		for (auto it = clArgs.begin(); it != clArgs.end();
 				++it) {
 			runtimeOptions->insert(
 					std::make_pair(it->first, it->second.as<std::string>()));
