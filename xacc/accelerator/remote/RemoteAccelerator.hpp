bfba6a354b999886f5d9f0b4444c3e91e6956f8c
Alex McCaskey <mccaskeyaj@ornl.gov>
2018-02-01 19:32:02 +0000

    adding cpprestsdk as externalproject, xacc lone dep is now openssl 1.0.2
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- xacc/accelerator/remote/RemoteAccelerator.hpp
+++ xacc/accelerator/remote/RemoteAccelerator.hpp
@@ -34,55 +34,12 @@ public:
 	virtual const std::string post(const std::string& remoteUrl,
 			const std::string& path, const std::string& postStr,
 			std::map<std::string, std::string> headers = std::map<std::string,
-					std::string> { }) {
-
-		if (headers.empty()) {
-			headers.insert(std::make_pair("Content-type", "application/json"));
-			headers.insert(std::make_pair("Connection", "keep-alive"));
-			headers.insert(std::make_pair("Accept", "*/*"));
-		}
-
-		http::uri uri = http::uri(remoteUrl);
-		http_client postClient(
-				http::uri_builder(uri).append_path(U(path)).to_uri());
-		http_request postRequest(methods::POST);
-		for (auto& kv : headers) {
-			postRequest.headers().add(kv.first, kv.second);
-		}
-		postRequest.set_body(postStr);
-
-		// Post the problem, get the response as json
-		auto postResponse = postClient.request(postRequest);
-		auto respJson = postResponse.get().extract_string().get();
-
-		// Map that response to a string
-		std::stringstream ss;
-		ss << respJson;
-		return ss.str();
-	}
+					std::string> { });
 
 	virtual const std::string get(const std::string& remoteUrl,
 			const std::string& path,
 			std::map<std::string, std::string> headers = std::map<std::string,
-					std::string> { }) {
-		http_client getClient(
-				http::uri_builder(http::uri(remoteUrl)).append_path(U(path)).to_uri());
-		http_request getRequest(methods::GET);
-		for (auto& kv : headers) {
-			getRequest.headers().add(kv.first, kv.second);
-		}
-		auto getResponse = getClient.request(getRequest);
-		// get the result as a string
-		std::stringstream z;
-		try {
-			z << getResponse.get().extract_json().get();
-//			xacc::info("GET RESPONSE: " + z.str());
-		} catch (std::exception& e) {
-			std::cout << "HELLO WORLD!, Caught exception, " << e.what() << "\n";
-		}
-
-		return z.str();
-	}
+					std::string> { });
 
 	virtual ~RestClient() {}
 
@@ -105,18 +62,7 @@ public:
 	 * @param function The kernel to execute.
 	 */
 	virtual void execute(std::shared_ptr<AcceleratorBuffer> buffer,
-				const std::shared_ptr<Function> function) {
-
-		auto jsonPostStr = processInput(buffer, std::vector<std::shared_ptr<Function>> {
-				function });
-
-		auto responseStr = restClient->post(remoteUrl, postPath, jsonPostStr, headers);
-
-		processResponse(buffer, responseStr);
-
-		return;
-
-	}
+				const std::shared_ptr<Function> function);
 
 	/**
 	 * Execute a set of kernels with one remote call. Return
@@ -130,13 +76,7 @@ public:
 	 */
 	virtual std::vector<std::shared_ptr<AcceleratorBuffer>> execute(
 			std::shared_ptr<AcceleratorBuffer> buffer,
-			const std::vector<std::shared_ptr<Function>> functions) {
-		auto jsonPostStr = processInput(buffer, functions);
-
-		auto responseStr = restClient->post(remoteUrl, postPath, jsonPostStr, headers);
-
-		return processResponse(buffer, responseStr);
-	}
+			const std::vector<std::shared_ptr<Function>> functions);
 
 	virtual bool isRemote() {
 		return true;
