c7723d24a5ec1c20b50df4a6dd4322cd9062705e
Alex McCaskey <mccaskeyaj@ornl.gov>
2018-03-27 11:37:53 -0400

    updating service registry to be more dynamic in its plugin installs
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- xacc/utils/ServiceRegistry.cpp
+++ xacc/utils/ServiceRegistry.cpp
@@ -1,27 +1,60 @@
 #include "ServiceRegistry.hpp"
 #include <boost/filesystem.hpp>
+#include <pybind11/embed.h>
 
 namespace xacc {
 
 void ServiceRegistry::initialize() {
 
 	if (!initialized) {
-//		XACCLogger::instance()->info("Creating CppUs Framework.");
 		framework = FrameworkFactory().NewFramework();
 
 		// Initialize the framework, such that we can call
 		// GetBundleContext() later.
-//		XACCLogger::instance()->info("Running Framework.Init.");
 		framework.Init();
 
-//		XACCLogger::instance()->info("Getting the Framework Bundle Context.");
 		context = framework.GetBundleContext();
 		if (!context) {
 			XACCLogger::instance()->error(
 					"Invalid XACC Framework plugin context.");
 		}
 
-		const std::string xaccLibDir = std::string(XACC_INSTALL_DIR) + std::string("/lib");
+		std::string xaccLibDir = "";
+#ifdef XACC_HAS_PYTHON
+		namespace py = pybind11;
+		auto getInstallDir = []() -> std::string {
+   		   auto locals = py::dict();
+   		   py::exec(R"(
+			import os
+			imported = True
+			try:
+			   import pyxacc as xacc
+			except ImportError:
+			   imported = False
+
+			if imported:
+			    xaccLocation = os.path.dirname(os.path.realpath(xacc.__file__))
+			    if os.path.basename(xaccLocation) == 'python':
+			       xaccLocation = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+			   )", py::globals(), locals);
+		   bool foundPythonInstall = locals["imported"].cast<bool>();
+		   if (foundPythonInstall) {
+		        return locals["xaccLocation"].cast<std::string>() + std::string("/lib");
+  		   } else {
+			XACCLogger::instance()->error("ServiceRegistry Error, invalid pyxacc install");
+			return "";
+		   }
+		};
+
+		if(!Py_IsInitialized()) {
+			py::scoped_interpreter guard{};
+			xaccLibDir = getInstallDir();
+		}  else {
+			xaccLibDir = getInstallDir();
+		}
+#else
+		xaccLibDir = std::string(XACC_INSTALL_DIR) + std::string("/lib");
+#endif
 
 		XACCLogger::instance()->enqueueLog("XACC Library Directory: " + xaccLibDir);
 		for (auto &entry : boost::filesystem::directory_iterator(xaccLibDir)) {
@@ -31,6 +64,7 @@ void ServiceRegistry::initialize() {
 				auto name = entry.path().filename().string();
 				boost::replace_all(name,"lib","");
 				boost::replace_all(name,".so","");
+				boost::replace_all(name,".dy","");
 				XACCLogger::instance()->enqueueLog("Installing base plugin " + name);
 				context.InstallBundles(entry.path().string());
 			}
@@ -38,8 +72,7 @@ void ServiceRegistry::initialize() {
 
 		// Add external plugins...
 		boost::filesystem::directory_iterator end_itr;
-		boost::filesystem::path xaccPluginPath(
-				std::string(XACC_INSTALL_DIR) + std::string("/lib/plugins"));
+		boost::filesystem::path xaccPluginPath(xaccLibDir + std::string("/plugins"));
 		if (boost::filesystem::exists(xaccPluginPath)) {
 			for (auto& entry : boost::filesystem::directory_iterator(xaccPluginPath)) {
 				auto p = entry.path();
@@ -48,6 +81,7 @@ void ServiceRegistry::initialize() {
 						auto name = subentry.path().filename().string();
 						boost::replace_all(name,"lib","");
 						boost::replace_all(name,".so","");
+						boost::replace_all(name,".dy","");
 						XACCLogger::instance()->enqueueLog("Installing 3rd party plugin " + name);
 						context.InstallBundles(subentry.path().string());
 					}
