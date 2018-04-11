669796e05c5a08994c849af1a369afba01bb0967
Alex McCaskey <mccaskeyaj@ornl.gov>
2018-04-11 18:10:02 +0000

    work to switch to restclient-cpp
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- xacc/accelerator/remote/RemoteAccelerator.cpp
+++ xacc/accelerator/remote/RemoteAccelerator.cpp
@@ -13,57 +13,37 @@
 #include "RemoteAccelerator.hpp"
 
 namespace xacc {
-const std::string RestClient::post(const std::string& remoteUrl,
+const std::string Client::post(const std::string& remoteUrl,
 		const std::string& path, const std::string& postStr,
 		std::map<std::string, std::string> headers) {
 
+	using namespace RestClient;
+	init();
+	Connection* conn = new Connection(remoteUrl);
+	HeaderFields _headers;
 	if (headers.empty()) {
 		headers.insert(std::make_pair("Content-type", "application/json"));
 		headers.insert(std::make_pair("Connection", "keep-alive"));
 		headers.insert(std::make_pair("Accept", "*/*"));
 	}
-
-	http::uri uri = http::uri(remoteUrl);
-	http_client_config config;
-	config.set_validate_certificates(false);
-	http_client postClient(
-			http::uri_builder(uri).append_path(U(path)).to_uri(), config);
-	http_request postRequest(methods::POST);
 	for (auto& kv : headers) {
-		postRequest.headers().add(kv.first, kv.second);
+		_headers[kv.first] = kv.second;
 	}
-	postRequest.set_body(postStr);
-
-//	xacc::info("POSTING: " + postStr + " to " + remoteUrl + ", " + path);// << "\n";
-
-	// Post the problem, get the response as json
-	auto postResponse = postClient.request(postRequest);
-	auto respJson = postResponse.get().extract_string().get();
-
-	// Map that response to a string
-	std::stringstream ss;
-	ss << respJson;
-//	xacc::info("post response was " + ss.str());// << "\n";
-	return ss.str();
+	conn->SetHeaders(headers);
+	Response r = conn->post(path, postStr);
+	if (r.code != 200) throw std::runtime_error("HTTP POST Error");
+	delete conn;
+	disable();
+	return r.body;
 }
 
-const std::string RestClient::get(const std::string& remoteUrl,
+const std::string Client::get(const std::string& remoteUrl,
 		const std::string& path, std::map<std::string, std::string> headers) {
-	http_client_config config;
-	config.set_validate_certificates(false);
-	http_client getClient(
-			http::uri_builder(http::uri(remoteUrl)).append_path(U(path)).to_uri(), config);
-	http_request getRequest(methods::GET);
-	for (auto& kv : headers) {
-		getRequest.headers().add(kv.first, kv.second);
-	}
-//	xacc::info("Getting IBM at " + remoteUrl + ", " + path);
-	auto getResponse = getClient.request(getRequest);
-	// get the result as a string
-	std::stringstream z;
-	z << getResponse.get().extract_json().get();
-	xacc::info("GET RESPONSE: " + z.str());
-	return z.str();
+
+	using namespace RestClient;
+	Response r = RestClient::get(remoteUrl+path);
+	if (r.code != 200) throw std::runtime_error("HTTP GET Error");
+	return r.body;
 }
 
 void RemoteAccelerator::execute(std::shared_ptr<AcceleratorBuffer> buffer,
