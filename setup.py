43c91e6a2ce010f4ff3733b4ffea87f31c28013d
Alex McCaskey <mccaskeyaj@ornl.gov>
2018-05-30 15:33:44 -0400

    Update setup.py
    update version to 0.1.2

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ class CMakeBuild(build_ext):
 
 s = setup(
     name='xacc',
-    version='0.1.0',
+    version='0.1.2',
     author='Alex McCaskey',
     author_email='xacc-dev@eclipse.org',
     packages=find_packages('python'),
