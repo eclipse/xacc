258b225551a3895e2971ed523e4ba4b95d149fbe
H. Charles Zhao <czhao39@gmail.com>
2018-07-20 09:23:32 -0400

    Add verbose option to plugin generator
    Signed-off-by: H. Charles Zhao <czhao39@gmail.com>

--- python/xaccplugingen/xaccplugingen.py
+++ python/xaccplugingen/xaccplugingen.py
@@ -19,6 +19,7 @@ def add_subparser(subparsers):
                                       formatter_class=argparse.RawDescriptionHelpFormatter)
     subparser.add_argument("-l", "--libname",
                            help="library name (e.g. xacc-quilcompiler, which would be the default if run with '-n QuilCompiler')")
+    subparser.add_argument("-v", "--verbose", help="increase verbosity", action="store_true")
 
     required_group = subparser.add_argument_group("required arguments")
     required_group.add_argument("-t", "--type", help="plugin type", required=True, choices=PLUGIN_TYPES)
@@ -35,25 +36,37 @@ def run_generator(args):
 
     if args.type.lower() == "pluginproject":
         template_dir = os.path.join(templates_dir, "pluginproject/")
+        if args.verbose:
+            print("Generating a plugin project...")
         generate_project(template_dir, output_dir, args)
     elif args.type.lower() == "compiler":
         template_dir = os.path.join(templates_dir, "compiler/")
+        if args.verbose:
+            print("Generating a compiler plugin...")
         generate_compiler(template_dir, output_dir, args)
 
 
-def generate(template_dir, output_dir, format_func):
+def generate(template_dir, output_dir, format_func, verbose=False):
     for dirpath, dirnames, filenames in os.walk(template_dir):
         dir_rel_path = os.path.relpath(dirpath, template_dir)
+        if verbose:
+            print("Entered {}/".format(dir_rel_path))
         for dirname in dirnames:
             outdir_name = format_func(dirname)
-            os.mkdir(os.path.join(output_dir, dir_rel_path, outdir_name))
+            outdir_path = os.path.join(output_dir, dir_rel_path, outdir_name)
+            os.mkdir(outdir_path)
+            if verbose:
+                print("Created {}".format(outdir_path))
         for filename in filenames:
             with open(os.path.join(dirpath, filename), 'r') as template_file:
                 contents = template_file.read()
             outfile_name = format_func(filename)
+            outfile_path = os.path.join(output_dir, dir_rel_path, outfile_name)
             contents = format_func(contents)
-            with open(os.path.join(output_dir, dir_rel_path, outfile_name), 'w') as outfile:
+            with open(outfile_path, 'w') as outfile:
                 outfile.write(contents)
+            if verbose:
+                print("Created {}".format(outfile_path))
 
 
 def generate_project(template_dir, output_dir, args):
@@ -61,7 +74,7 @@ def generate_project(template_dir, output_dir, args):
         project_name=args.libname,
         project_shortname_upper=args.name.upper(),
         project_shortname_lower=args.name.lower()
-    ))
+    ), verbose=args.verbose)
 
 
 def generate_compiler(template_dir, output_dir, args):
@@ -70,4 +83,4 @@ def generate_compiler(template_dir, output_dir, args):
         class_name=args.name,
         class_name_lower=args.name.lower(),
         class_name_upper=args.name.upper()
-    ))
+    ), verbose=args.verbose)
