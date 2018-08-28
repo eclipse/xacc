0848db86495bd031ef39c971261c4dcfe61c561c
Zachary Parks <1zp@ornl.gov>
2018-08-28 16:41:24 -0400

    Modified GateFunctionTester.cpp to reflect the parameter insertion changes
    Signed-off-by: Zachary Parks <1zp@ornl.gov>

--- quantum/gate/ir/tests/GateFunctionTester.cpp
+++ quantum/gate/ir/tests/GateFunctionTester.cpp
@@ -4,247 +4,227 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * and Eclipse Distribution License v1.0 which accompanies this
  * distribution. The Eclipse Public License is available at
- * http://www.eclipse.org/legal/epl-v10.html and the Eclipse Distribution License
- * is available at https://eclipse.org/org/documents/edl-v10.php
+ * http://www.eclipse.org/legal/epl-v10.html and the Eclipse Distribution
+ *License is available at https://eclipse.org/org/documents/edl-v10.php
  *
  * Contributors:
  *   Alexander J. McCaskey - initial API and implementation
  *******************************************************************************/
-#include <gtest/gtest.h>
+#include "CNOT.hpp"
 #include "GateFunction.hpp"
 #include "Hadamard.hpp"
-#include "CNOT.hpp"
-#include "Rz.hpp"
-#include "Ry.hpp"
-#include "Rx.hpp"
-#include "X.hpp"
 #include "InstructionIterator.hpp"
+#include "Rx.hpp"
+#include "Ry.hpp"
+#include "Rz.hpp"
+#include "X.hpp"
+#include <gtest/gtest.h>
 
 using namespace xacc::quantum;
 
-const std::string expectedQasm =
-		"H qreg1\n"
-		"CNOT qreg1,qreg2\n"
-		"CNOT qreg0,qreg1\n"
-		"H qreg0\n";
+const std::string expectedQasm = "H qreg1\n"
+                                 "CNOT qreg1,qreg2\n"
+                                 "CNOT qreg0,qreg1\n"
+                                 "H qreg0\n";
 
-TEST(GateFunctionTester,checkFunctionMethods) {
+TEST(GateFunctionTester, checkFunctionMethods) {
 
-	GateFunction f("foo");
-	EXPECT_TRUE(f.name() == "foo");
-	EXPECT_TRUE(f.nInstructions() == 0);
+  GateFunction f("foo");
+  EXPECT_TRUE(f.name() == "foo");
+  EXPECT_TRUE(f.nInstructions() == 0);
 
-	auto h = std::make_shared<Hadamard>(1);
-	auto cn1 = std::make_shared<CNOT>(1, 2);
-	auto cn2 = std::make_shared<CNOT>(0, 1);
-	auto h2 = std::make_shared<Hadamard>(0);
+  auto h = std::make_shared<Hadamard>(1);
+  auto cn1 = std::make_shared<CNOT>(1, 2);
+  auto cn2 = std::make_shared<CNOT>(0, 1);
+  auto h2 = std::make_shared<Hadamard>(0);
 
-	f.addInstruction(h);
-	f.addInstruction(cn1);
-	f.addInstruction(cn2);
-	f.addInstruction(h2);
-	EXPECT_TRUE(f.nInstructions() == 4);
-	EXPECT_TRUE(f.toString("qreg") == expectedQasm);
+  f.addInstruction(h);
+  f.addInstruction(cn1);
+  f.addInstruction(cn2);
+  f.addInstruction(h2);
+  EXPECT_TRUE(f.nInstructions() == 4);
+  EXPECT_TRUE(f.toString("qreg") == expectedQasm);
 
-	f.removeInstruction(0);
-	EXPECT_TRUE(f.nInstructions() == 3);
+  f.removeInstruction(0);
+  EXPECT_TRUE(f.nInstructions() == 3);
 
-	const std::string removeExpectedQasm =
-			"CNOT qreg1,qreg2\n"
-			"CNOT qreg0,qreg1\n"
-			"H qreg0\n";
-	EXPECT_TRUE(f.toString("qreg") == removeExpectedQasm);
+  const std::string removeExpectedQasm = "CNOT qreg1,qreg2\n"
+                                         "CNOT qreg0,qreg1\n"
+                                         "H qreg0\n";
+  EXPECT_TRUE(f.toString("qreg") == removeExpectedQasm);
 
+  f.replaceInstruction(0, h);
+  const std::string replaceExpectedQasm = "H qreg1\n"
+                                          "CNOT qreg0,qreg1\n"
+                                          "H qreg0\n";
+  EXPECT_TRUE(f.toString("qreg") == replaceExpectedQasm);
 
-	f.replaceInstruction(0, h);
-	const std::string replaceExpectedQasm = "H qreg1\n"
-			"CNOT qreg0,qreg1\n"
-			"H qreg0\n";
-	EXPECT_TRUE(f.toString("qreg") == replaceExpectedQasm);
-
-	f.replaceInstruction(2, cn1);
-	const std::string replaceExpectedQasm2 = "H qreg1\n"
-			"CNOT qreg0,qreg1\n"
-			"CNOT qreg1,qreg2\n";
-	EXPECT_TRUE(f.toString("qreg") == replaceExpectedQasm2);
-
+  f.replaceInstruction(2, cn1);
+  const std::string replaceExpectedQasm2 = "H qreg1\n"
+                                           "CNOT qreg0,qreg1\n"
+                                           "CNOT qreg1,qreg2\n";
+  EXPECT_TRUE(f.toString("qreg") == replaceExpectedQasm2);
 }
 
-TEST(GateFunctionTester,checkWalkFunctionTree) {
-	auto f = std::make_shared<GateFunction>("foo");
-	auto g = std::make_shared<GateFunction>("goo");
+TEST(GateFunctionTester, checkWalkFunctionTree) {
+  auto f = std::make_shared<GateFunction>("foo");
+  auto g = std::make_shared<GateFunction>("goo");
 
-	auto h = std::make_shared<Hadamard>(1);
-	auto cn1 = std::make_shared<CNOT>(1, 2);
-	auto cn2 = std::make_shared<CNOT>(0, 1);
-	auto h2 = std::make_shared<Hadamard>(0);
+  auto h = std::make_shared<Hadamard>(1);
+  auto cn1 = std::make_shared<CNOT>(1, 2);
+  auto cn2 = std::make_shared<CNOT>(0, 1);
+  auto h2 = std::make_shared<Hadamard>(0);
 
-	g->addInstruction(cn1);
-	g->addInstruction(cn2);
+  g->addInstruction(cn1);
+  g->addInstruction(cn2);
 
-	f->addInstruction(h);
-	f->addInstruction(g);
-	f->addInstruction(h2);
+  f->addInstruction(h);
+  f->addInstruction(g);
+  f->addInstruction(h2);
 
-	xacc::InstructionIterator it(f);
-	while(it.hasNext()) {
-		auto inst = it.next();
-		if (!inst->isComposite()) std::cout << inst->name() << "\n";
-	}
-
+  xacc::InstructionIterator it(f);
+  while (it.hasNext()) {
+    auto inst = it.next();
+    if (!inst->isComposite())
+      std::cout << inst->name() << "\n";
+  }
 }
 
-TEST(GateFunctionTester,checkEvaluateVariables) {
+TEST(GateFunctionTester, checkEvaluateVariables) {
 
-	xacc::Initialize();
-	xacc::InstructionParameter p("phi");
-	xacc::InstructionParameter fParam("phi");
+  xacc::InstructionParameter p("phi");
+  xacc::InstructionParameter fParam("phi");
 
-	EXPECT_TRUE(p == fParam);
+  EXPECT_TRUE(p == fParam);
 
-	auto rz = std::make_shared<Rz>(std::vector<int>{0});
-	rz->setParameter(0, p);
+  auto rz = std::make_shared<Rz>(std::vector<int>{0});
+  rz->setParameter(0, p);
 
-	GateFunction f("foo", std::vector<xacc::InstructionParameter>{fParam});
+  GateFunction f("foo", std::vector<xacc::InstructionParameter>{fParam});
 
-	f.addInstruction(rz);
+  f.addInstruction(rz);
 
-	std::cout << f.toString("qreg") << "\n";
+  std::cout << f.toString("qreg") << "\n";
 
-	Eigen::VectorXd v(1);
-	v(0) = 3.1415;
+  Eigen::VectorXd v(1);
+  v(0) = 3.1415;
 
-	auto evaled = f(v);
+  auto evaled = f(v);
 
-	EXPECT_TRUE(boost::get<double>(evaled->getInstruction(0)->getParameter(0)) == 3.1415);
+  EXPECT_TRUE(boost::get<double>(evaled->getInstruction(0)->getParameter(0)) ==
+              3.1415);
 
-	std::cout << "ParamSet:\n" << f.toString("qreg") << "\n";
+  std::cout << "ParamSet:\n" << f.toString("qreg") << "\n";
 
-	v(0) = 6.28;
+  v(0) = 6.28;
 
-	evaled = f(v);
+  evaled = f(v);
 
-	std::cout << "ParamSet:\n" << evaled->toString("qreg") << "\n";
-
-	xacc::Finalize();
+  std::cout << "ParamSet:\n" << evaled->toString("qreg") << "\n";
 }
 
-TEST(GateFunctionTester, checkParameterInsertion){
+TEST(GateFunctionTester, checkParameterInsertion) {
 
-    xacc::Initialize();
-    xacc::InstructionParameter p("phi");
-    xacc::InstructionParameter p2("psi");
+  xacc::InstructionParameter p1("theta");
+  xacc::InstructionParameter p2("1 * theta");
+  xacc::InstructionParameter p3("0.5 * psi");
 
-    auto ry = std::make_shared<Ry>(std::vector<int>{0});
-    auto rz = std::make_shared<Rz>(std::vector<int>{1});
+  auto rz = std::make_shared<Rz>(std::vector<int>{1});
+  auto ry = std::make_shared<Ry>(std::vector<int>{2});
+  auto rx = std::make_shared<Rx>(std::vector<int>{3});
 
-    ry->setParameter(0, p);
-    rz->setParameter(0, p2);
+  rz->setParameter(0, p1);
+  ry->setParameter(0, p2);
+  rx->setParameter(0, p3);
 
-    GateFunction f("foo");
-    f.addInstruction(ry);
-    f.addInstruction(rz);
-    // Testing if AddInstruction adds parameters to GateFunction parameter vector
-    EXPECT_TRUE(f.nParameters() == 2);
-    EXPECT_TRUE(f.getParameter(0).which() == 3);
-    EXPECT_TRUE(f.getParameter(1).which() == 3);
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "phi");
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(1)) == "psi");
+  GateFunction f("foo");
 
-    auto rx = std::make_shared<Rx>(std::vector<int>{2});
+  f.addInstruction(rz);
+  f.addInstruction(ry);
 
-    rx->setParameter(0, p);
+  EXPECT_TRUE(f.nInstructions() == 2);
+  EXPECT_TRUE(f.nParameters() == 1);
+  EXPECT_TRUE(f.toString("qreg") == "Rz(theta) qreg1\nRy(1 * theta) qreg2\n");
 
-    f.addInstruction(rx);
-    // Testing if adding instruction with same parameter changes number of parameters
-    EXPECT_TRUE(f.nParameters() == 2);
+  // replace duplicated parameter with a unique one
+  f.replaceInstruction(1, rx);
 
-    f.removeInstruction(0);
-    // Testing if removing instruction that has duplicate parameter keeps the parameter
-    EXPECT_TRUE(f.nInstructions() == 2);
-    EXPECT_TRUE(f.nParameters() == 2);
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "phi");
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(1)) == "psi");
+  EXPECT_TRUE(f.nInstructions() == 2);
+  EXPECT_TRUE(f.nParameters() == 2);
+  EXPECT_TRUE(f.toString("qreg") == "Rz(theta) qreg1\nRx(0.5 * psi) qreg3\n");
+  EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "theta");
+  EXPECT_TRUE(boost::get<std::string>(f.getParameter(1)) == "psi");
 
-    f.removeInstruction(0);
+  // replace unique Instruction parameter with a duplicated parameter
+  f.replaceInstruction(1, ry);
 
-    EXPECT_TRUE(f.nInstructions() == 1);
-    EXPECT_TRUE(f.getInstruction(0)->name() == "Rx");
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "phi");
-    // Testing insertInstruction keeps parameter integrity of GateFunction
-    f.insertInstruction(0, rz);
+  EXPECT_TRUE(f.nInstructions() == 2);
+  EXPECT_TRUE(f.nParameters() == 1);
+  EXPECT_TRUE(f.toString("qreg") == "Rz(theta) qreg1\nRy(1 * theta) qreg2\n");
+  EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "theta");
+  // make sure parameters get removed completely
+  f.removeInstruction(0);
+  EXPECT_TRUE(f.nParameters() == 1);
+  f.removeInstruction(0);
 
-    EXPECT_TRUE(f.nInstructions() == 2);
-    EXPECT_TRUE(f.getInstruction(0)->name() == "Rz");
-    EXPECT_TRUE(f.getInstruction(1)->name() == "Rx");
-    EXPECT_TRUE(f.nParameters() == 2);
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "phi");
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(1)) == "psi");
+  EXPECT_TRUE(f.nInstructions() == 0);
+  EXPECT_TRUE(f.nParameters() == 0);
 
-    xacc::InstructionParameter p3("theta");
+  auto x = std::make_shared<X>(std::vector<int>{2});
 
-    auto ry2 = std::make_shared<Ry>(std::vector<int>{3});
-    ry2->setParameter(0, p3);
+  f.addInstruction(x);
 
-    f.replaceInstruction(1, ry2);
-    // Testing if replaceInstruction keeps parameter integrity of GateFunction
-    EXPECT_TRUE(f.nInstructions() == 2);
-    EXPECT_TRUE(f.getInstruction(0)->name() == "Rz");
-    EXPECT_TRUE(f.getInstruction(1)->name() == "Ry");
-    EXPECT_TRUE(f.nParameters() == 2);
-    std::cout << "PARAMETER(0) = :" << boost::get<std::string>(f.getParameter(0)) << std::endl;
-    std::cout << "PARAMETER(1) = :" << boost::get<std::string>(f.getParameter(1)) << std::endl;
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "theta");
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(1)) == "psi");
+  EXPECT_TRUE(f.nParameters() == 0);
+  EXPECT_TRUE(f.nInstructions() == 1);
 
-    auto h = std::make_shared<Hadamard>(1);
-    auto x = std::make_shared<X>(1);
+  f.addInstruction(rz);
+  // replace an Instruction without a parameter with a parameterized Instruction
+  f.replaceInstruction(0, rx);
 
-    f.replaceInstruction(1, h);
+  EXPECT_TRUE(f.nParameters() == 2);
+  EXPECT_TRUE(f.nInstructions() == 2);
+  EXPECT_TRUE(f.toString("qreg") == "Rx(0.5 * psi) qreg3\nRz(theta) qreg1\n");
+  EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "theta");
+  EXPECT_TRUE(boost::get<std::string>(f.getParameter(1)) == "psi");
 
-    EXPECT_TRUE(f.nInstructions() == 2);
-    EXPECT_TRUE(f.getInstruction(0)->name() == "Rz");
-    EXPECT_TRUE(f.getInstruction(1)->name() == "H");
-    EXPECT_TRUE(f.nParameters() == 1);
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "psi");
+  // replace a parameterized Instruction with one that does not have a parameter
+  f.replaceInstruction(0, x);
 
-    f.replaceInstruction(1, x);
+  EXPECT_TRUE(f.nParameters() == 1);
+  EXPECT_TRUE(f.nInstructions() == 2);
+  EXPECT_TRUE(f.toString("qreg") == "X qreg2\nRz(theta) qreg1\n");
+  EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "theta");
 
-    EXPECT_TRUE(f.nInstructions() == 2);
-    EXPECT_TRUE(f.getInstruction(0)->name() == "Rz");
-    EXPECT_TRUE(f.getInstruction(1)->name() == "X");
+  f.replaceInstruction(0, rx);
 
-    EXPECT_TRUE(f.nParameters() == 1);
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "psi");
+  f.addInstruction(ry);
 
-    f.replaceInstruction(1, ry2);
+  EXPECT_TRUE(f.nParameters() == 2);
+  EXPECT_TRUE(f.nInstructions() == 3);
+  EXPECT_TRUE(f.toString("qreg") ==
+              "Rx(0.5 * psi) qreg3\nRz(theta) qreg1\nRy(1 * theta) qreg2\n");
+  EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "theta");
+  EXPECT_TRUE(boost::get<std::string>(f.getParameter(1)) == "psi");
 
-    EXPECT_TRUE(f.nInstructions() == 2);
-    EXPECT_TRUE(f.getInstruction(0)->name() == "Rz");
-    EXPECT_TRUE(f.getInstruction(1)->name() == "Ry");
-    EXPECT_TRUE(f.nParameters() == 2);
+  auto rx2 = std::make_shared<Rx>(std::vector<int>{4});
+  xacc::InstructionParameter p5("1 * phi");
+  rx2->setParameter(0, p5);
+  // replacing duplicated parameter with a brand new parameter
+  f.replaceInstruction(1, rx2);
 
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "psi");
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(1)) == "theta");
+  EXPECT_TRUE(f.nParameters() == 3);
+  EXPECT_TRUE(f.nInstructions() == 3);
+  EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "theta");
+  EXPECT_TRUE(boost::get<std::string>(f.getParameter(1)) == "psi");
+  EXPECT_TRUE(boost::get<std::string>(f.getParameter(2)) == "phi");
 
-    xacc::InstructionParameter p4("1 * theta");
-    
-    auto rx2 = std::make_shared<Rx>(std::vector<int>{3});
-
-    rx2->setParameter(0, p4);
-
-    f.addInstruction(rx2);
-
-    EXPECT_TRUE(f.nInstructions() == 3);
-    EXPECT_TRUE(f.nParameters() == 2);
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(0)) == "psi");
-    EXPECT_TRUE(boost::get<std::string>(f.getParameter(1)) == "theta");
-
-    xacc::Finalize();
-
-
+  EXPECT_TRUE(f.toString("qreg") ==
+              "Rx(0.5 * psi) qreg3\nRx(1 * phi) qreg4\nRy(1 * theta) qreg2\n");
 }
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-   return RUN_ALL_TESTS();
+
+int main(int argc, char **argv) {
+  xacc::Initialize();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+  xacc::Finalize();
 }
