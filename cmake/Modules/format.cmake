7433320c1316e18e1ab82cee2888afe4d390dfeb
Zachary Parks <1zp@ornl.gov>
2018-09-05 10:03:18 -0400

    Fixing permissions for format script, including format target creation on condition of clang-format installation, updating xacc-config.cmake.in
    Signed-off-by: Zachary Parks <1zp@ornl.gov>

--- cmake/Modules/format.cmake
+++ cmake/Modules/format.cmake
@@ -8,18 +8,18 @@ if(NOT CLANG_FORMAT_EXECUTABLE)
         if(CLANG_FORMAT_EXECUTABLE)
                 message("-- Found clang-format: ${CLANG_FORMAT_EXECUTABLE}")
         else()
-                message(FATAL_ERROR "-- clang-format not found!")
+                message("-- clang-format not found! 'format' target not built. Install clang-format to enable formatting.")
         endif()
 else()
    message("-- Using clang-format: ${CLANG_FORMAT_EXECUTABLE}")
    if(NOT EXISTS ${CLANG_FORMAT_EXECUTABLE})
         message(FATAL_ERROR "-- clang-format path is invalid!")
+   else()
+        add_custom_target(format
+              COMMAND
+                  CLANG_FORMAT_EXE=${CLANG_FORMAT_EXECUTABLE}
+                  ${CMAKE_SOURCE_DIR}/scripts/check_format_cpp.sh --apply-patch
+              WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+)
    endif()
 endif()
-
-add_custom_target(format
-        COMMAND
-            CLANG_FORMAT_EXE=${CLANG_FORMAT_EXECUTABLE}
-            ${CMAKE_SOURCE_DIR}/scripts/check_format_cpp.sh --apply-patch
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-)
