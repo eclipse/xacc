ef076c33f001cd2243cf661ad5c39ccc08c9562a
Alex McCaskey <mccaskeyaj@ornl.gov>
2018-09-07 11:40:07 -0400

    fixing a few bugs in clang format work
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- cmake/Modules/format.cmake
+++ cmake/Modules/format.cmake
@@ -1,25 +1,18 @@
-if(NOT CLANG_FORMAT_EXECUTABLE)
-        find_program(CLANG_FORMAT_EXECUTABLE
-           NAMES
+find_program(CLANG_FORMAT_EXECUTABLE
+    NAMES
 	   clang-format-7.0
 	   clang-format-mp-7.0
 	   clang-format
-        )
-        if(CLANG_FORMAT_EXECUTABLE)
-                message("-- Found clang-format: ${CLANG_FORMAT_EXECUTABLE}")
-        else()
-                message("-- clang-format not found! 'format' target not built. Install clang-format to enable formatting.")
-        endif()
-else()
-   message("-- Using clang-format: ${CLANG_FORMAT_EXECUTABLE}")
-   if(NOT EXISTS ${CLANG_FORMAT_EXECUTABLE})
-        message(FATAL_ERROR "-- clang-format path is invalid!")
-   else()
-        add_custom_target(format
+)
+if(CLANG_FORMAT_EXECUTABLE)
+    message("-- Found clang-format: ${CLANG_FORMAT_EXECUTABLE}")
+     add_custom_target(format
               COMMAND
                   CLANG_FORMAT_EXE=${CLANG_FORMAT_EXECUTABLE}
                   ${CMAKE_SOURCE_DIR}/scripts/check_format_cpp.sh --apply-patch
               WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-)
-   endif()
+     )
+else()
+    message("-- clang-format not found! 'format' target not built. Install clang-format to enable formatting.")
 endif()
+
