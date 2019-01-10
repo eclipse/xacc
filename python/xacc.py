7289ad00a4b14dfc9fbaf75918d2a2504dc87b41
Zachary Parks <1zp@ornl.gov>
2019-01-10 15:49:47 +0000

    changes to Python decorator API and benchmark framework
    Signed-off-by: Zachary Parks <1zp@ornl.gov>

--- python/xacc.py
+++ python/xacc.py
@@ -14,20 +14,22 @@ except:
     pass
 
 canExecuteBenchmarks = True
-try:    
+try:
    import pelix.framework
    from pelix.utilities import use_service
+   from pelix.ipopo.decorators import (ComponentFactory, Property, Requires,
+                                        Provides, Instantiate)
    from abc import abstractmethod, ABC
    import configparser
    class Algorithm(ABC):
-    
+
       # Override this execute method to implement the algorithm
       # @input inputParams
-      # @return buffer 
+      # @return buffer
       @abstractmethod
       def execute(self, inputParams):
          pass
-    
+
       # Override this analyze method called to manipulate result data from executing the algorithm
       # @input buffer
       # @input inputParams
@@ -37,8 +39,8 @@ try:
 
 except:
    canExecuteBenchmarks = False
-   pass 
-    
+   pass
+
 
 def parse_args(args):
     parser = argparse.ArgumentParser(description="XACC Framework Utility.",
@@ -60,22 +62,19 @@ def parse_args(args):
                         help="The IBM Q Project.", required=False)
     parser.add_argument("-L", "--location", action='store_true',
                         help="Print the path to the XACC install location.", required=False)
-    parser.add_argument("-f", "--framework-help", action='store_true',
-                        help="Print the help information for XACC and its available plugins.", required=False)
     parser.add_argument("--python-include-dir", action='store_true',
                         help="Print the path to the Python.h.", required=False)
     parser.add_argument("-b", "--branch", default='master', type=str,
                         help="Print the path to the XACC install location.", required=False)
     parser.add_argument("--benchmark", type=str, help="Run the benchmark detailed in the given input file.", required=False)
-    parser.add_argument("--list-backends", type=str, help="List the backends available for the provided Accelerator.", required=False)
-    
+
     if hasPluginGenerator:
        subparsers = parser.add_subparsers(title="subcommands", dest="subcommand",
                                           description="Run python3 -m xacc [subcommand] -h for more information about a specific subcommand")
        plugin_generator.add_subparser(subparsers)
-    
+
        opts = parser.parse_args(args)
-    
+
     if opts.set_credentials and not opts.api_key:
         print('Error in arg input, must supply api-key if setting credentials')
         sys.exit(1)
@@ -157,9 +156,13 @@ def setCredentials(opts):
 
 def qasm2Kernel(kernelName, qasmStr):
     return '__qpu__ {}(AcceleratorBuffer b) {{\n {} \n}}'.format(kernelName, qasmStr)
-    
-class WrappedF(object):
-    def __init__(self, f, *args, **kwargs):
+
+class decoratorFunction(ABC):
+
+    def __init__(self):
+        pass
+
+    def initialize(self, f, *args, **kwargs):
         self.function = f
         self.args = args
         self.kwargs = kwargs
@@ -168,7 +171,7 @@ class WrappedF(object):
 
     def overrideAccelerator(self, acc):
         self.accelerator = acc
-        
+
     def nParameters(self):
         if 'accelerator' in self.kwargs:
             if isinstance(self.kwargs['accelerator'], Accelerator):
@@ -215,47 +218,51 @@ class WrappedF(object):
         compiledKernel = program.getKernels()[0]
         return compiledKernel.getIRFunction()
 
+    @abstractmethod
     def __call__(self, *args, **kwargs):
+        self.src = '\n'.join(inspect.getsource(self.function).split('\n')[1:])
+        compiler = getCompiler('xacc-py')
         if self.accelerator == None:
             if 'accelerator' in self.kwargs:
                 if isinstance(self.kwargs['accelerator'], Accelerator):
-                    qpu = self.kwargs['accelerator']
+                    self.qpu = self.kwargs['accelerator']
                 else:
-                    qpu = getAccelerator(self.kwargs['accelerator'])
+                    self.qpu = getAccelerator(self.kwargs['accelerator'])
             elif hasAccelerator('tnqvm'):
-                qpu = getAccelerator('tnqvm')
+                self.qpu = getAccelerator('tnqvm')
             else:
                 print(
                     '\033[1;31mError, no Accelerators installed. We suggest installing TNQVM.\033[0;0m')
                 exit(0)
         else:
-            print('hello world, setting ', self.accelerator.name())
-            qpu = self.accelerator
-            
-        # Remove the @qpu line from the source
-        src = '\n'.join(inspect.getsource(self.function).split('\n')[1:])
+            print('Setting accelerator: ', self.accelerator.name())
+            self.qpu = self.accelerator
+        ir = compiler.compile(self.src, self.qpu)
+        program = Program(self.qpu, ir)
+        self.compiledKernel = program.getKernels()[0]
+        pass
 
+class WrappedF(decoratorFunction):
+
+    def __init__(self, f, *args, **kwargs):
+        self.function = f
+        self.args = args
+        self.kwargs = kwargs
+        self.__dict__.update(kwargs)
+        self.accelerator = None
+
+    def __call__(self, *args, **kwargs):
+        super().__call__(*args, **kwargs)
         argsList = list(args)
         if not isinstance(argsList[0], AcceleratorBuffer):
             raise RuntimeError(
                 'First argument of an xacc kernel must be the Accelerator Buffer to operate on.')
-
         buffer = argsList[0]
-
         functionBufferName = inspect.getargspec(self.function)[0][0]
-
         # Replace function arg0 name with buffer.name()
-        src = src.replace(functionBufferName, buffer.name())
-
-        # Get the compiler and compile the code
-        compiler = getCompiler('xacc-py')
-        ir = compiler.compile(src, qpu)
-        program = Program(qpu, ir)
-        compiledKernel = program.getKernels()[0]
-
-        compiledKernel.execute(argsList[0], argsList[1:])
+        self.src = self.src.replace(functionBufferName, buffer.name())
+        self.compiledKernel.execute(argsList[0], argsList[1:])
         return
-
 
 class qpu(object):
     def __init__(self, *args, **kwargs):
@@ -265,7 +272,14 @@ class qpu(object):
         return
 
     def __call__(self, f):
-        return WrappedF(f, *self.args, **self.kwargs)
+        if 'algo' in self.kwargs:
+            servName = self.kwargs['algo']
+            function = serviceRegistry.get_service('decorator_algorithm_service', servName)
+            function.initialize(f, *self.args, **self.kwargs)
+            return function
+        else:
+            return WrappedF(f, *self.args, **self.kwargs)
+
 
 def compute_readout_error_probabilities(qubits, buffer, qpu, shots=8192, persist=True):
     p10Functions = []
@@ -273,7 +287,7 @@ def compute_readout_error_probabilities(qubits, buffer, qpu, shots=8192, persist
     p10CheckedBitStrings = [] # ['001','010','100'] for 3 bits
     p10s = []
     p01s = []
-    
+
     zeros = '0'*buffer.size()
     for i, q in enumerate(qubits):
         measure = gate.create('Measure',[q],[i])
@@ -283,7 +297,7 @@ def compute_readout_error_probabilities(qubits, buffer, qpu, shots=8192, persist
         tmp = list(zeros)
         tmp[buffer.size()-1-q] = '1'
         p10CheckedBitStrings.append(''.join(tmp))
-    
+
     for i, q in enumerate(qubits):
         x = gate.create('X',[q])
         measure = gate.create('Measure',[q],[i])
@@ -291,10 +305,10 @@ def compute_readout_error_probabilities(qubits, buffer, qpu, shots=8192, persist
         f.addInstruction(x)
         f.addInstruction(measure)
         p01Functions.append(f)
-        
+
     setOption(qpu.name()+'-shots', shots)
-    
-    # Execute 
+
+    # Execute
     b1 = qpu.createBuffer('tmp1',max(qubits)+1)
     b2 = qpu.createBuffer('tmp2',max(qubits)+1)
 
@@ -311,18 +325,18 @@ def compute_readout_error_probabilities(qubits, buffer, qpu, shots=8192, persist
     if persist:
         if not os.path.exists(os.getenv('HOME')+'/.xacc_cache/ro_characterization'):
             os.makedirs(os.getenv('HOME')+'/.xacc_cache/ro_characterization')
-        
+
         backend = 'NullBackend'
         if optionExists(qpu.name()+'-backend'):
             backend = getOption(qpu.name()+'-backend')
-            
+
         filename = os.getenv('HOME')+'/.xacc_cache/ro_characterization/'+qpu.name()+'_'+backend+"_ro_error_{}.json".format(time.ctime().replace(' ','_').replace(':','_'))
-        
+
         data = {'shots':shots, 'backend':backend}
         for i in qubits:
             data[str(i)] = {'0|1':p01s[i],'1|0':p10s[i],'+':(p01s[i]+p10s[i]),'-':(p01s[i]-p10s[i])}
         with open(filename,'w') as outfile: json.dump(data, outfile)
-        
+
     return p01s,p10s, filename
 
 
@@ -347,33 +361,32 @@ class PyServiceRegistry(object):
             "pelix.ipopo.core",
             "pelix.shell.console"))
         framework.start()
-        self.context = framework.get_bundle_context()        
-            
-    # Setup bundle directory and bundles as well as installs them
-    # it might even make sense to just move this to __init__()
-    
+        self.context = framework.get_bundle_context()
+        self.services = []
     def initialize(self):
+        serviceList = ['decorator_algorithm_service', 'benchmark_algorithm_service']
+        #self.context.install_bundle('generic').start()
         xaccLocation = os.path.dirname(os.path.realpath(__file__))
         pluginDir = xaccLocation + '/py-plugins'
         if not os.path.exists(pluginDir):
             os.makedirs(pluginDir)
-
         sys.path.append(pluginDir)
 
         pluginFiles = [f for f in os.listdir(
             pluginDir) if os.path.isfile(os.path.join(pluginDir, f))]
         for f in pluginFiles:
             bundle_name = os.path.splitext(f)[0].strip()
-            print(bundle_name, f)
             self.context.install_bundle(bundle_name).start()
+        try:
+            for servType in serviceList:
+                self.services += self.context.get_all_service_references(servType)
+        except:
+            if len(self.services) > 0:
+                pass
 
-        services = self.context.get_all_service_references(
-            'xacc_algorithm_service')
-        if services is None:
-            print("No XACC benchmark algorithm bundles found in " + pluginDir + ".")
+        if not self.services:
+            print("No XACC algorithm bundles found in " + pluginDir + ".")
             exit(1)
-        for s in services:
-            info('[XACC-Benchmark] Algorithm Service: %s is installed.' % s.get_properties()['name'])
 
     def get_service(self, serviceName, name):
         services = self.context.get_all_service_references(serviceName)
@@ -382,35 +395,35 @@ class PyServiceRegistry(object):
             if s.get_properties()['name'] == name:
                 service = self.context.get_service(s)
         return service
-    
+
 def benchmark(opts):
     # Now instantiation == initialization
     serviceRegistry = PyServiceRegistry()
-    
-    if opts.benchmark is not None:      
+
+    if opts.benchmark is not None:
         inputfile = opts.benchmark
         xacc_settings = process_benchmark_input(inputfile)
     else:
         error('Must provide input file for benchmark.')
         return
-    
+
     serviceRegistry.initialize()
     Initialize()
-    
+
     if ':' in xacc_settings['accelerator']:
         accelerator, backend = xacc_settings['accelerator'].split(':')
         setOption(accelerator + "-backend", backend)
         xacc_settings['accelerator'] = accelerator
-    
+
     accelerator = xacc_settings['accelerator']
     if 'n-shots' in xacc_settings:
         if 'local-ibm' in accelerator:
             accelerator = 'ibm'
         setOption(accelerator+('-trials' if 'rigetti' in accelerator else '-shots'), xacc_settings['n-shots'])
-        
-    # Using ServiceRegistry to getService (xacc_algorithm_service) and execute the service
+
+    # Using ServiceRegistry to getService (benchmark_algorithm_service) and execute the service
     algorithm = serviceRegistry.get_service(
-        'xacc_algorithm_service', xacc_settings['algorithm'])
+        'benchmark_algorithm_service', xacc_settings['algorithm'])
     if algorithm is None:
         print("XACC algorithm service with name " +
                    xacc_settings['algorithm'] + " is not installed.")
@@ -448,6 +461,9 @@ def process_benchmark_input(filename):
         print("Input file " + filename + " could not be opened.")
         exit(1)
 
+serviceRegistry = PyServiceRegistry()
+serviceRegistry.initialize()
+
 def main(argv=None):
     opts = parse_args(sys.argv[1:])
     xaccLocation = os.path.dirname(os.path.realpath(__file__))
@@ -458,28 +474,14 @@ def main(argv=None):
     if hasPluginGenerator and opts.subcommand == "generate-plugin":
         plugin_generator.run_generator(opts, xaccLocation)
         sys.exit(0)
-        
+
     if opts.python_include_dir:
         print(sysconfig.get_paths()['platinclude'])
         sys.exit(0)
 
-    if opts.framework_help is not None:
-        Initialize(['--help'])
-        return
-        
-    if opts.list_backends is not None:
-        acc = opts.list_backends
-        if acc == 'ibm':
-            info('Retrieving remote IBM backend information')
-            Initialize(['--'+acc+'-list-backends'])
-        elif acc == 'dwave':
-            info('Retrieving remote D-Wave solver information')
-            Initialize(['--'+acc+'-list-solvers'])
-        return
-
     if not opts.set_credentials == None:
         setCredentials(opts)
-    
+
     if not opts.benchmark == None:
         if not canExecuteBenchmarks:
             error('Cannot execute benchmark. Please install configparser and ipopo.')
