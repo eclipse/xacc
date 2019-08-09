d99f6df05d55df1230b8235f6f811e53f392b811
Alex McCaskey <mccaskeyaj@ornl.gov>
2019-08-09 09:15:08 -0400

    updates to range generator to include start and end keys
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- quantum/plugins/generators/range/range.cpp
+++ quantum/plugins/generators/range/range.cpp
@@ -6,11 +6,19 @@ using namespace xacc;
 namespace xacc {
 namespace generators {
 bool Range::validateOptions() {
-  if (options.count("nqubits") && options.count("gate")) {
-    return true;
+  if (!options.count("gate")) {
+    return false;
   }
-  return false;
+
+  if (!options.count("nqubits")) {
+    if (!options.count("start") && !options.count("end")) {
+      return false;
+    }
+  }
+
+  return true;
 }
+
 std::shared_ptr<xacc::Function>
 Range::generate(std::map<std::string, xacc::InstructionParameter> &parameters) {
 
@@ -19,16 +27,32 @@ Range::generate(std::map<std::string, xacc::InstructionParameter> &parameters) {
   }
 
   auto gate = options["gate"].as<std::string>();
-  int nqubits;
-  try {
-     nqubits = options["nqubits"].as_no_error<int>();
-  } catch (std::exception& e) {
-      nqubits = (int) options["nqubits"].as<double>();
+  int start = 0, end, nqubits;
+  if (options.count("nqubits")) {
+    try {
+      nqubits = options["nqubits"].as_no_error<int>();
+    } catch (std::exception &e) {
+      nqubits = (int)options["nqubits"].as<double>();
+    }
+    end = nqubits;
+  } else if (options.count("start")) {
+    try {
+      start = options["start"].as_no_error<int>();
+    } catch (std::exception &e) {
+      start = (int)options["start"].as<double>();
+    }
+
+    try {
+      end = options["end"].as_no_error<int>();
+    } catch (std::exception &e) {
+      end = (int)options["end"].as<double>();
+    }
+
   }
 
   auto provider = xacc::getIRProvider("quantum");
   auto f = provider->createFunction("range" + gate, {});
-  for (int i = 0; i < nqubits; i++) {
+  for (int i = start; i < end; i++) {
     auto g = provider->createInstruction(gate, {i});
     f->addInstruction(g);
   }
