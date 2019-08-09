2c27773a90ef1cfa41ab9edf0367fcb7dabf5efb
Alex McCaskey <mccaskeyaj@ornl.gov>
2019-08-09 09:15:08 -0400

    updates to range generator to include start and end keys
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- quantum/plugins/generators/range/tests/RangeTester.cpp
+++ quantum/plugins/generators/range/tests/RangeTester.cpp
@@ -10,8 +10,12 @@ TEST(ExpTester, checkSimple) {
   auto f =
       exp->generate({{"gate","H"}, {"nqubits",10}});
 
-      std::cout << "F:\n" << f->toString() << "\n";
-   EXPECT_EQ(10, f->nInstructions());
+  std::cout << "F:\n" << f->toString() << "\n";
+  EXPECT_EQ(10, f->nInstructions());
+
+  auto g = exp->generate({{"gate","H"}, {"start",3},{"end",10}});
+  std::cout << "G:\n" << g->toString() << "\n";
+  EXPECT_EQ(7, g->nInstructions());
 }
 
 int main(int argc, char **argv) {
