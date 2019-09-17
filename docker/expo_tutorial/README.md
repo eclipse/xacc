1e52bd52977a9e50dd20cd4f799948e6f2a7ab11
Alex McCaskey <mccaskeyaj@ornl.gov>
2019-09-17 09:04:56 -0400

    updating cmakelists licenses
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- docker/expo_tutorial/README.md
+++
@@ -1,8 +0,0 @@
-Tutorial instructions
-
-```bash
-$ docker-compose pull
-$ docker-compose up -d
-```
-Open localhost:9000 in web browser
-
--- scripts/check_format_cpp.sh
+++ scripts/check_format_cpp.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# *******************************************************************************
+# Copyright (c) 2019 UT-Battelle, LLC.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# and Eclipse Distribution License v.10 which accompany this distribution.
+# The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+# and the Eclipse Distribution License is available at
+# https://eclipse.org/org/documents/edl-v10.php
+#
+# Contributors:
+#   Alexander J. McCaskey - initial API and implementation
+# *******************************************************************************/
+
 clang_format_executable=${CLANG_FORMAT_EXE:-clang-format}
 
 this_program=$(basename "$0")
