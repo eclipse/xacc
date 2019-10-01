565f3433c94fc196385c9c3a8ef3099f2bbe7e62
Alex McCaskey <mccaskeyaj@ornl.gov>
2019-10-01 16:00:40 -0400

    started work on updates to qcs accelerator to use zeromq and msgpack
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- quantum/plugins/rigetti/qcs/QCSAccelerator.cpp
+++ quantum/plugins/rigetti/qcs/QCSAccelerator.cpp
@@ -13,19 +13,24 @@
 #include "QCSAccelerator.hpp"
 #include <algorithm>
 #include "CountGatesOfTypeVisitor.hpp"
-#include <pybind11/numpy.h>
+#include "QuilVisitor.hpp"
 
+#include "messages.hpp"
+#include "xacc.hpp"
 #include "xacc_service.hpp"
 #include "EmbeddingAlgorithm.hpp"
 
 #include <regex>
 #include <chrono>
+#include <uuid/uuid.h>
+
+#include <zmq.hpp>
 
 using namespace std;
 using namespace std::chrono;
 
-namespace py = pybind11;
-using namespace py::literals;
+// namespace py = pybind11;
+// using namespace py::literals;
 
 namespace xacc {
 namespace quantum {
@@ -44,7 +49,7 @@ std::shared_ptr<IR> MapToPhysical::transform(std::shared_ptr<IR> ir) {
 
   for (auto &i : nUniqueBits) {
     physical2Logical.insert({i, counter});
-    logical2Physical.insert({counter,i});
+    logical2Physical.insert({counter, i});
     counter++;
   }
 
@@ -52,7 +57,7 @@ std::shared_ptr<IR> MapToPhysical::transform(std::shared_ptr<IR> ir) {
 
   auto hardwareGraph = xacc::getService<Graph>("boost-ugraph");
   for (int i = 0; i < nBits; i++) {
-    std::map<std::string, InstructionParameter> m{{"bias", 1.0}};
+    HeterogeneousMap m{std::make_pair("bias", 1.0)};
     hardwareGraph->addVertex(m);
   }
   for (auto &edge : _edges) {
@@ -60,8 +65,8 @@ std::shared_ptr<IR> MapToPhysical::transform(std::shared_ptr<IR> ir) {
                            physical2Logical[edge.second]);
   }
 
-  //hardwareGraph->write(std::cout);
-  for (auto &function : ir->getKernels()) {
+  // hardwareGraph->write(std::cout);
+  for (auto &function : ir->getComposites()) {
     auto logicalGraph = function->toGraph();
     InstructionIterator it(function);
     std::set<int> nUniqueProbBits;
@@ -71,9 +76,9 @@ std::shared_ptr<IR> MapToPhysical::transform(std::shared_ptr<IR> ir) {
       // Get the next node in the tree
       auto nextInst = it.next();
       if (nextInst->isEnabled() && nextInst->bits().size() == 2) {
-          probEdges.push_back({nextInst->bits()[0], nextInst->bits()[1]});
-          nUniqueProbBits.insert(nextInst->bits()[0]);
-          nUniqueProbBits.insert(nextInst->bits()[1]);
+        probEdges.push_back({nextInst->bits()[0], nextInst->bits()[1]});
+        nUniqueProbBits.insert(nextInst->bits()[0]);
+        nUniqueProbBits.insert(nextInst->bits()[1]);
       }
     }
 
@@ -81,68 +86,47 @@ std::shared_ptr<IR> MapToPhysical::transform(std::shared_ptr<IR> ir) {
     auto problemGraph = xacc::getService<Graph>("boost-ugraph");
 
     for (int i = 0; i < nProbBits; i++) {
-      std::map<std::string, InstructionParameter> m{{"bias", 1.0}};
+    HeterogeneousMap m{std::make_pair("bias", 1.0)};
       problemGraph->addVertex(m);
     }
 
     for (auto &inst : probEdges) {
       if (!problemGraph->edgeExists(inst.first, inst.second)) {
-         problemGraph->addEdge(inst.first, inst.second, 1.0);
+        problemGraph->addEdge(inst.first, inst.second, 1.0);
       }
     }
 
-//    std::cout << "\n";
-  //  problemGraph->write(std::cout);
+    //    std::cout << "\n";
+    //  problemGraph->write(std::cout);
 
     // Compute the minor graph embedding
     auto embedding = embeddingAlgorithm->embed(problemGraph, hardwareGraph);
-//    embedding.persist(std::cout);
+    //    embedding.persist(std::cout);
     std::vector<int> physicalMap;
-    for (auto& kv : embedding) {
-        if (kv.second.size() > 1) {
-            xacc::error("Invalid logical to physical qubit mapping.");
-        }
-        physicalMap.push_back(logical2Physical[kv.second[0]]);
+    for (auto &kv : embedding) {
+      if (kv.second.size() > 1) {
+        xacc::error("Invalid logical to physical qubit mapping.");
+      }
+      physicalMap.push_back(logical2Physical[kv.second[0]]);
     }
 
     // std::cout << "Physical bits:\n";
     // for (auto& b : physicalMap) std::cout << b << "\n";
-    function->mapBits(physicalMap);
-
+    // function->mapBits(physicalMap);
   }
 
   return ir;
 }
 
-std::shared_ptr<AcceleratorBuffer>
-QCSAccelerator::createBuffer(const std::string &varId) {
-  auto buffer = std::make_shared<AcceleratorBuffer>(varId, 30);
-  storeBuffer(varId, buffer);
-  return buffer;
-}
-
-std::shared_ptr<AcceleratorBuffer>
-QCSAccelerator::createBuffer(const std::string &varId, const int size) {
-  std::shared_ptr<AcceleratorBuffer> buffer;
-  buffer = std::make_shared<AcceleratorBuffer>(varId, size);
-  storeBuffer(varId, buffer);
-  return buffer;
-}
-
-bool QCSAccelerator::isValidBufferSize(const int NBits) { return NBits > 0; }
-
-void QCSAccelerator::execute(std::shared_ptr<AcceleratorBuffer> buffer,
-                             const std::shared_ptr<Function> function) {
+void QCSAccelerator::execute(
+    std::shared_ptr<AcceleratorBuffer> buffer,
+    const std::shared_ptr<CompositeInstruction> function) {
   auto visitor = std::make_shared<QuilVisitor>(true);
 
-  auto backend = xacc::optionExists("qcs-backend")
-                     ? xacc::getOption("qcs-backend")
-                     : "9q-generic-qvm";
-  auto shots = xacc::optionExists("qcs-shots")
-                   ? std::stoi(xacc::getOption("qcs-shots"))
-                   : 1000;
+  auto shots = 1024;
 
-  std::map<int, int> bitToQubit;
+      std::map<int, int>
+          bitToQubit;
   std::vector<int> tobesorted;
   InstructionIterator it(function);
   while (it.hasNext()) {
@@ -150,96 +134,123 @@ void QCSAccelerator::execute(std::shared_ptr<AcceleratorBuffer> buffer,
     auto nextInst = it.next();
     if (nextInst->isEnabled()) {
       nextInst->accept(visitor);
-      if (nextInst->name() == "Measure") {
-        bitToQubit.insert(
-            {mpark::get<int>(nextInst->getParameter(0)), nextInst->bits()[0]});
-        tobesorted.push_back(nextInst->bits()[0]);
-      }
     }
   }
 
-  // std::sort(tobesorted.begin(),tobesorted.end());
-  // bitToQubit.clear();
-  // for (int i = 0; i < tobesorted.size(); i++) {
-  //    bitToQubit[i] = tobesorted[i];
-  //}
-
-  // Create our usual old Quil string, but
-  // then hack it to fit the new DECLARE stuff
   CountGatesOfTypeVisitor<Measure> count(function);
   int nMeasures = count.countGates();
   auto quilStr = visitor->getQuilString();
-  //   quilStr = std::regex_replace(quilStr, std::regex("["), "ro[");
-  //   boost::replace_all(quilStr, "[", "ro[");
   quilStr =
       "DECLARE ro BIT[" + std::to_string(buffer->size()) + "]\n" + quilStr;
 
+  const string endpoint = "tcp://127.0.0.1:5555";
+  zmq::context_t context;
+  zmq::socket_t socket(context, zmq::socket_type::dealer);
+  socket.connect(endpoint);
 
-// std::shared_ptr<pybind11::scoped_interpreter> guard;
+  uuid_t uuid;
+  uuid_generate_time_safe(uuid);
+  char uuid_str[37]; // ex. "1b4e28ba-2fa1-11d2-883f-0016d3cca427" + "\0"
+  uuid_unparse_lower(uuid, uuid_str);
 
+  std::string id(uuid_str);
+  qcs::BinaryExecutableRequest binExecReq(shots, quilStr);
+  qcs::BinaryExecutableParams params(binExecReq);
+  qcs::RPCRequestBinaryExecutable r(id, params);
 
+  msgpack::sbuffer sbuf;
+  msgpack::pack(sbuf, r);
+  zmq::message_t msg(sbuf.size());
+  memcpy(msg.data(), sbuf.data(), sbuf.size());
 
-    // py::print("quil:\n", quilStr);
-  auto pyquil = py::module::import("pyquil");
-  py::object get_qc = pyquil.attr("get_qc");
-  auto program = pyquil.attr("Program")();
-  program.attr("inst")(quilStr);
-  program.attr("wrap_in_numshots_loop")(shots);
+  std::cout << msg << "\n";
 
-  auto qc = get_qc(backend);
-  auto compiled = qc.attr("compile")(program);
+  socket.send(msg);
 
-  py::array_t<int> results = qc.attr("run")(compiled);
-  auto shape = results.request().shape;
-//   py::print(shape[0]);
-//   py::print(shape[1]);
-//   py::print(results);
-  //  py::print("QUIL\n");
-  // py::print(quilStr);
-  //py::print(buffer->size());
-  std::string zeroString = "";
-  for (int i = 0; i < shape[1]; i++)
-    zeroString += "0";
+  zmq::message_t reply;
+  socket.recv(&reply, 0);
+  std::cout << reply.data() << "\n";
+  msgpack::unpacked unpackedData;
+  msgpack::unpack(unpackedData, static_cast<const char *>(reply.data()),
+                  reply.size());
+  std::stringstream ss;
+  ss << unpackedData.get();
 
-  for (int i = 0; i < shots; i++) {
-    std::string bitString = zeroString;
-    std::stringstream s;
+  auto prog = "";
 
-    for (int j = 0; j < shape[1]; j++) {
-      //   auto qbit = bitToQubit[j];
-      s << *results.data(i, j);
-      //   bitString[buffer->size()-1-qbit] = s.str()[0];
-    }
-    bitString = s.str();
-    std::reverse(bitString.begin(), bitString.end());
-    //    if (i<4) py::print("adding " + bitString);
-    buffer->appendMeasurement(bitString);
-  }
+  std::cout << "GOT THE JSON:\n" << ss.str() << "\n";
+
+  // QPU REQUEST
+  qcs::QPURequest qpuReq(prog, id);
+  qcs::QPURequestParams qpuParams(qpuReq);
+  qcs::RPCRequestQPURequest r2(id, qpuParams);
+  msgpack::sbuffer sbuf2;
+  msgpack::pack(sbuf2, r2);
+  zmq::message_t msg2(sbuf.size());
+  memcpy(msg2.data(), sbuf2.data(), sbuf2.size());
+
+  std::cout << msg2 << "\n";
+
+  socket.send(msg2);
+
+  zmq::message_t reply2;
+  socket.recv(&reply2, 0);
+  std::cout << reply2.data() << "\n";
+  msgpack::unpacked unpackedData2;
+  msgpack::unpack(unpackedData2, static_cast<const char *>(reply2.data()),
+                  reply2.size());
+  std::stringstream ss2;
+  ss2 << unpackedData2.get();
+
+  auto waitId = "";
+  // GET BUFFERS
+  qcs::GetBuffersRequest getBuffers(waitId);
+  qcs::RPCRequestGetBuffers r3(id, getBuffers);
+
+  msgpack::sbuffer sbuf3;
+  msgpack::pack(sbuf3, r3);
+  zmq::message_t msg3(sbuf.size());
+  memcpy(msg3.data(), sbuf3.data(), sbuf3.size());
+
+  std::cout << msg3 << "\n";
+
+  socket.send(msg3);
+
+  zmq::message_t reply3;
+  socket.recv(&reply3, 0);
+  std::cout << reply3.data() << "\n";
+  msgpack::unpacked unpackedData3;
+  msgpack::unpack(unpackedData3, static_cast<const char *>(reply3.data()),
+                  reply3.size());
+  std::stringstream ss3;
+  ss3 << unpackedData3.get();
+
+  // now we have json results
 
   return;
 }
 
-std::vector<std::shared_ptr<AcceleratorBuffer>> QCSAccelerator::execute(
+void QCSAccelerator::execute(
     std::shared_ptr<AcceleratorBuffer> buffer,
-    const std::vector<std::shared_ptr<Function>> functions) {
+    const std::vector<std::shared_ptr<CompositeInstruction>> functions) {
 
-  std::vector<std::shared_ptr<AcceleratorBuffer>> tmpBuffers;
-  int counter = 1;
-  for (auto f : functions) {
-    auto tmpBuffer = createBuffer(f->name(), buffer->size());
+  //   std::vector<std::shared_ptr<AcceleratorBuffer>> tmpBuffers;
+  //   int counter = 1;
+  //   for (auto f : functions) {
+  //     auto tmpBuffer = createBuffer(f->name(), buffer->size());
 
-    high_resolution_clock::time_point t1 = high_resolution_clock::now();
-    // xacc::info("Execution " + std::to_string(counter) + ": " + f->name());
-    execute(tmpBuffer, f);
-    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+  //     high_resolution_clock::time_point t1 = high_resolution_clock::now();
+  //     // xacc::info("Execution " + std::to_string(counter) + ": " +
+  //     f->name()); execute(tmpBuffer, f); high_resolution_clock::time_point t2
+  //     = high_resolution_clock::now();
 
-    auto duration = duration_cast<microseconds>(t2 - t1).count();
-    tmpBuffer->addExtraInfo("exec-time", ExtraInfo(duration * 1e-6));
-    tmpBuffers.push_back(tmpBuffer);
-    counter++;
-  }
+  //     auto duration = duration_cast<microseconds>(t2 - t1).count();
+  //     tmpBuffer->addExtraInfo("exec-time", ExtraInfo(duration * 1e-6));
+  //     tmpBuffers.push_back(tmpBuffer);
+  //     counter++;
+  //   }
 
-  return tmpBuffers;
+  //   return tmpBuffers;
 }
 
 } // namespace quantum
