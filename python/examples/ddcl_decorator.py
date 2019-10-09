3be8e84b347abb0e0009b867f62d69ffad076587
Alex McCaskey <mccaskeyaj@ornl.gov>
2019-10-09 11:31:34 -0400

    adding ddcl decorator example
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

---
+++ python/examples/ddcl_decorator.py
@@ -0,0 +1,18 @@
+import xacc
+
+# Get the QPU and allocate a single qubit
+qpu = xacc.getAccelerator('local-ibm')
+qbits = xacc.qalloc(1)
+
+@xacc.qpu(algo='ddcl', accelerator=qpu, optimizer='mlpack', target_dist=[.5,.5], loss='js', gradient='js-parameter-shift')
+def f(qbits, x, y, z):
+    Ry(qbits[0], x)
+    Ry(qbits[0], y)
+    Ry(qbits[0], z)
+
+f(qbits, 0, 0, 0)
+
+print(qbits.getInformation('opt-val'))
+
+params = qbits.getAllUnique('parameters')
+print(params)
