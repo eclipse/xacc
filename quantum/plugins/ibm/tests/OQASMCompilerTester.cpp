596009578eb5d18561dd4c057b91e0c07bd8fb1d
Alex McCaskey <mccaskeyaj@ornl.gov>
2019-10-25 14:30:37 -0400

    fixing bug in openqasm compiler grammar spec
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- quantum/plugins/ibm/tests/OQASMCompilerTester.cpp
+++ quantum/plugins/ibm/tests/OQASMCompilerTester.cpp
@@ -22,7 +22,7 @@ using namespace xacc::quantum;
 
 TEST(OQasmCompilerTester, checkCompileOQASM) {
 
-    const std::string src("__qpu__ nonsense(AcceleratorBuffer qregister, double n) {\n"
+    const std::string src("__qpu__ void nonsense(qbit qregister, double n) {\n"
         "   // nonsense\n"
         "   OPENQASM 2.0;\n"
         "   qreg q[3];\n"
@@ -47,23 +47,19 @@ TEST(OQasmCompilerTester, checkCompileOQASM) {
         //"post q[2];\n"
         "   measure q[2] -> c[2];\n"
         "}\n"
-        "__qpu__ morenonsense(AcceleratorBuffer ab, double n) {\n"
-        "   // nonsense 2\n"
-        "   OPENQASM 2.0;\n"
-        "   nonsense(ab, n);\n"
-        "   }\n"
+
         );
 
     auto compiler = xacc::getService<Compiler>("openqasm");
     auto ir = compiler->compile(src);
 
-    EXPECT_TRUE(ir->getComposites().size() == 2);
+    EXPECT_TRUE(ir->getComposites().size() == 1);
 
     auto function = ir->getComposite("nonsense");
     std::cout << "HELLO\n" << function->toString() << "\n";
     std::cout << "N: " << function->nInstructions() << std::endl;
 
-    EXPECT_TRUE(ir->getComposites().size() == 2);
+    EXPECT_TRUE(ir->getComposites().size() == 1);
 
     auto hadamardVisitor = std::make_shared<CountGatesOfTypeVisitor<Hadamard>>(function);
     auto cnotVisitor = std::make_shared<CountGatesOfTypeVisitor<CNOT>>(function);
@@ -84,6 +80,50 @@ TEST(OQasmCompilerTester, checkCompileOQASM) {
     // xacc::Finalize();
 }
 
+TEST(OQasmCompilerTester, checkComplex) {
+
+    const std::string src = R"src(include "qelib1.inc";
+qreg q[4];
+creg c[4];
+u3(3.442869371886, -pi/2, pi/2) q[0];
+u1(2.671120676205) q[0];
+u3(1.540494492206, -pi/2, pi/2) q[1];
+u1(3.290440112129) q[1];
+u3(0.022294485029, -pi/2, pi/2) q[2];
+u1(0.436279645426) q[2];
+u3(3.572738737062, -pi/2, pi/2) q[3];
+u1(4.867597859102) q[3];
+cx q[0], q[1];
+cx q[0], q[2];
+cx q[0], q[3];
+u1(2.58848772445) q[0];
+u3(0.194413641661, -pi/2, pi/2) q[0];
+u1(3.639810972471) q[0];
+u1(4.912787893839) q[1];
+u3(3.079155348405, -pi/2, pi/2) q[1];
+u1(0.04721288765) q[1];
+u1(0.515801553473) q[2];
+u3(3.574647607134, -pi/2, pi/2) q[2];
+u1(0.68888718156) q[2];
+u1(3.832419100678) q[3];
+u3(5.969537803768, -pi/2, pi/2) q[3];
+u1(4.238742400331) q[3];
+cx q[0], q[1];
+cx q[0], q[2];
+cx q[0], q[3];
+u1(1.98824294549) q[0];
+u3(0.507174280394, -pi/2, pi/2) q[0];
+u1(4.537931482297) q[1];
+u3(1.540871188987, -pi/2, pi/2) q[1];
+u1(0.78718157196) q[2];
+u3(5.472312765418, -pi/2, pi/2) q[2];
+u1(2.028198126855) q[3];
+u3(1.059840546465, -pi/2, pi/2) q[3];
+measure q[0] -> c[0];
+measure q[1] -> c[1];
+measure q[2] -> c[2];
+measure q[3] -> c[3];)src";
+}
 int main(int argc, char** argv) {
         xacc::Initialize();
 
