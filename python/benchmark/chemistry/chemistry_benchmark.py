4cce0467d2897d6feae3609da46a86132538fe35
A.M Santana <santanaam@ornl.gov>
2019-11-19 13:02:47 -0500

    Adding chemistry benchmarks with example config files
    Signed-off-by: A.M Santana <santanaam@ornl.gov>

---
+++ python/benchmark/chemistry/chemistry_benchmark.py
@@ -0,0 +1,70 @@
+import xacc
+import ast
+
+from xacc import Benchmark
+from pelix.ipopo.decorators import ComponentFactory, Property, Requires, Provides, \
+Validate, Invalidate, Instantiate
+
+
+@ComponentFactory("chemistry_benchmark_factory")
+@Provides("benchmark")
+@Property("_benchmark", "benchmark", "chemistry")
+@Property("_name", "name", "chemistry")
+@Instantiate("chemistry_benchmark_instance")
+class Chemistry(Benchmark):
+
+    def execute(self, inputParams):
+        qpu = xacc.getAccelerator(inputParams['XACC']['accelerator'], {'shots':inputParams['XACC']['shots'] if 'shots' in inputParams else 1024})
+
+        H = None
+        if inputParams['Observable']['name'] == 'pauli':
+            obs_str = inputParams['Observable']['obs_str']
+            H = xacc.getObservable('pauli', obs_str)
+
+        elif inputParams['Observable']['name'] == 'fermion':
+            obs_str = inputParams['Observable']['obs_str']
+            H = xacc.getObservable('fermion', obs_str)
+
+        elif inputParams['Observable']['name'] == 'psi4-frozen-core':
+            basis = inputParams['Observable']['basis']
+            geometry = inputParams['Observable']['geometry']
+            fo = ast.literal_eval(inputParams['Observable']['fo'])
+            ao = ast.literal_eval(inputParams['Observable']['ao'])
+            H = xacc.getObservable('psi4-frozen-core', {'basis': basis,'geometry': geometry,
+                                                        'frozen-spin-orbitals': fo, 'active-spin-orbitals': ao})
+
+        #print('Ham: ', H)
+
+        buffer = xacc.qalloc(H.nBits())
+        optimizer = xacc.getOptimizer(inputParams['Optimizer']['name'])
+
+        provider = xacc.getIRProvider('quantum')
+
+        if inputParams['Ansatz']['name'] == 'xasm':
+            print('in here')
+            src = inputParams['Ansatz']['source']
+            xacc.qasm(src)
+            ansatz = xacc.getCompiled(inputParams['Ansatz']['circuit_name'])
+            # ansatz.expand(xacc.HeterogeneousMap())
+            print(ansatz.toString())
+        else:
+            print('here instead')
+            ansatz = provider.createInstruction(inputParams['Ansatz']['ansatz'])
+            ansatz = xacc.asComposite(ansatz)
+
+        alg = xacc.getAlgorithm(inputParams['Benchmark']['algorithm'], {
+                                'ansatz': ansatz,
+                                'accelerator': qpu,
+                                'observable': H,
+                                'optimizer': optimizer,
+                                })
+
+        alg.execute(buffer)
+        return buffer
+
+
+    def analyze(self, buffer, inputParams):
+        # Use to manipulate the resulting data from execution
+        print('Energy = ', buffer['opt-val'])
+        print('Opt Params = ', buffer['opt-params'])
+
