a443de6e068bcf99f739120e6df0132eb81f6d00
Alex McCaskey <mccaskeyaj@ornl.gov>
2019-12-11 12:59:18 -0500

    major cleanup on chemistry benchmark. improvements to aer_accelerator
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- python/plugins/aer/aer_accelerator.py
+++ python/plugins/aer/aer_accelerator.py
@@ -45,23 +45,13 @@ class AerAccelerator(xacc.Accelerator):
         qobjStr = self.qobj_compiler.translate(program)
         import json
         from qiskit import Aer
+        from qiskit.qobj.qobj import QasmQobjSchema
         from qiskit.qobj import (QasmQobj, QobjHeader,
                          QasmQobjInstruction,
                          QasmQobjExperiment, QasmQobjExperimentConfig, QobjExperimentHeader, QasmQobjConfig)
         qobj_json = json.loads(qobjStr)
-
-        # Create the Experiments using qiskit data structures
-        exps = [QasmQobjExperiment(config=QasmQobjExperimentConfig(memory_slots=e['config']['memory_slots'], n_qubits=e['config']['n_qubits']),
-                    header=QobjExperimentHeader(clbit_labels=e['header']['clbit_labels'],creg_sizes=e['header']['creg_sizes'],
-                                                memory_slots=e['header']['memory_slots'],n_qubits=e['header']['n_qubits'],
-                                                name=e['header']['name'], qreg_sizes=e['header']['qreg_sizes'],
-                                                qubit_labels=e['header']['qubit_labels']),
-                    instructions=[QasmQobjInstruction(name=i['name'], qubits=i['qubits'], params=(i['params'] if 'params' in i else [])) if i['name'] != 'measure'
-                                    else QasmQobjInstruction(name=i['name'], qubits=i['qubits'], memory=i['memory']) for i in e['instructions']]) for e in qobj_json['qObject']['experiments']]
-
-        qobj = QasmQobj(qobj_id=qobj_json['qObject']['qobj_id'],
-                        header=QobjHeader(), config=QasmQobjConfig(shots=self.shots, memory_slots=qobj_json['qObject']['config']['memory_slots']), experiments=exps, shots=self.shots)
-
+        qobj = QasmQobjSchema().load(qobj_json['qObject'])
+        exps = qobj.experiments
         measures = {}
         for i in exps[0].instructions:
             if i.name == "measure":
@@ -75,7 +65,6 @@ class AerAccelerator(xacc.Accelerator):
             job_sim = backend.run(qobj)
 
         sim_result = job_sim.result()
-
 
         for b,c in sim_result.get_counts().items():
             bitstring = b
