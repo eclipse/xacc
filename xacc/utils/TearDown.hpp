2e21a15a5a3496ffab9c1ccc6760cd9ea0cfbd45
Thien Nguyen <nguyentm@ornl.gov>
2020-01-17 10:33:58 -0500

    Add TearDown interface to support cleaning up resources at Finalize()
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

---
+++ xacc/utils/TearDown.hpp
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2020 UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompanies this
+ * distribution. The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html and the Eclipse Distribution
+ *License is available at https://eclipse.org/org/documents/edl-v10.php
+ *
+ * Contributors:
+ * Alexander J. McCaskey - initial API and implementation
+ * Thien Nguyen - initial API and implementation
+ *******************************************************************************/
+
+#pragma once
+
+// TearDown interface: all registered TearDown services 
+// will have its tearDown() method called once xacc is about to Finalize().
+// Contributing services (e.g. plugins) can use this to clean up any global resources
+// that they instantiated.
+namespace xacc {
+class TearDown {
+public:
+    virtual void tearDown() = 0;
+};
+} //namespace
