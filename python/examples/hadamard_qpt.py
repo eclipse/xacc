799d3b2f4e923c10b1eecc0eda9c75ae3b9b0344
Alex McCaskey <mccaskeyaj@ornl.gov>
2020-02-05 13:30:25 -0500

    Adding calculate method to Algorithm, implemented for fidelity key on qpt, adding python example
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

---
+++ python/examples/hadamard_qpt.py
@@ -0,0 +1,36 @@
+import xacc
+import numpy as np
+
+# Choose the QPU on which to
+# characterize the process matrix for a Hadamard
+qpu = xacc.getAccelerator('aer')
+
+# Create the CompositeInstruction containing a
+# single Hadamard instruction
+provider = xacc.getIRProvider('quantum')
+circuit = provider.createComposite('U')
+hadamard = provider.createInstruction('H', [0])
+circuit.addInstruction(hadamard)
+
+# Create the Algorithm, give it the circuit
+# to characterize and the backend to target
+qpt = xacc.getAlgorithm('qpt', {'circuit':circuit, 'accelerator':qpu})
+
+# Allocate a qubit, this will
+# store our tomography results
+buffer = xacc.qalloc(1)
+
+# Execute
+qpt.execute(buffer)
+
+# Compute the fidelity with respect to
+# the ideal hadamard process
+F = qpt.calculate('fidelity', buffer, {'chi-theoretical-real':[0., 0., 0., 0., 0., 1., 0., 1., 0., 0., 0., 0., 0., 1., 0., 1.]})
+print('\nFidelity: ', F)
+
+# Can get the chi process matrix and print it
+chi_real_part = np.array(buffer['chi-real'])
+chi_imag_part = np.array(buffer['chi-imag'])
+chi = np.reshape(chi_real_part + 1j*chi_imag_part, (4,4))
+print('\nChi:\n', chi)
+
