893313212cae3b081af532e3191584a9680c422e
Thien Nguyen <nguyentm@ornl.gov>
2020-02-20 14:32:18 -0500

    Working example of GOAT
    This is still *Work-in-progress* at the moment.
    In particular, I'm using a simple L-BFGS lib for testing purposes.
    Interestingly enough, both NL-OPT and ML-PACK failed to optimize (using l-bfgs)
    this example. They were simply stuck. All impls followed the same trajectory but only
    the simple one was able to escape the local minima and hence converge.
    
    Want to get this checked-in as the baseline first while diving into why ML-PACK and NL-OPT
    cannot work out-of-box for this case. Perhaps there are some options that we can tweak.
    
    Tested by: the added unit test for simple X gate optimization
    
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- quantum/plugins/optimal_control/GOAT.cpp
+++ quantum/plugins/optimal_control/GOAT.cpp
@@ -1,86 +1,247 @@
-#include <GOAT.hpp>
+#include "GOAT.hpp"
+#include <iostream>
 
-GOAT_PulseOptim::GOAT_PulseOptim(const Matrix& in_targetU, const Hamiltonian& in_hamiltonian, const dHdalpha& in_dHda,
-    IStopper* io_stopper, IIntegrator* io_integrator, IGradientStepper* io_gradStepper):
+namespace {
+constexpr int DEFAULT_NUMBER_STEPS = 10000;
+class RungeKutta
+{
+public:
+    RungeKutta(std::function<Matrix(float, const Matrix&)> in_func): 
+        m_func(in_func) 
+    {}
+
+    // Simple RK stepper
+    Matrix step(double in_time, const Matrix& in_mat, double in_dt) const
+    {
+        Matrix k1, k2, k3, k4;
+        k1 = m_func(in_time, in_mat);
+        k2 = m_func(in_time + in_dt / 2, in_mat + (in_dt / 2) * k1);
+        k3 = m_func(in_time + in_dt / 2, in_mat + (in_dt / 2) * k2);
+        k4 = m_func(in_time + in_dt, in_mat + in_dt * k3);
+
+        return in_mat + (k1 + 2 * k2 + 2 * k3 + k4) * in_dt / 6;
+    }
+
+private:
+    std::function<Matrix(float, const Matrix&)> m_func;
+};
+
+Matrix vstack(const Matrix& in_mat1, const Matrix& in_mat2)
+{
+    if(in_mat1.rows() == 0)
+    {
+        return in_mat2;
+    }
+    else if(in_mat2.rows() == 0)
+    {
+        return in_mat1;
+    }
+
+    auto nbCols = in_mat1.cols();
+    
+    if(nbCols == 0)
+    {
+        nbCols = in_mat2.cols();
+    }
+
+    Matrix result(in_mat1.rows()+in_mat2.rows(), nbCols);
+    result << in_mat1, in_mat2;
+  
+    return result;
+}
+}
+
+
+GOAT_PulseOptim::GOAT_PulseOptim(const Matrix& in_targetU, const Hamiltonian& in_hamiltonian, const dHdalpha& in_dHda, 
+    const OptimParams& in_initialParams, double in_maxTime, 
+    std::unique_ptr<IIntegrator>&& io_integrator, std::unique_ptr<IGradientStepper>&& io_gradStepper):
     m_targetU(in_targetU),
     m_hamiltonian(in_hamiltonian),
     m_dHda(in_dHda),
-    m_stopper(io_stopper),
-    m_integrator(io_integrator),
-    m_gradStepper(io_gradStepper)
-    {}
-
-OptimParams GOAT_PulseOptim::optimize(const OptimParams& in_initialParams, double in_maxTime, double& out_finalCost)
+    m_integrator(std::move(io_integrator)),
+    m_gradStepper(std::move(io_gradStepper)),
+    m_initialParams(in_initialParams),
+    m_maxTime(in_maxTime),
+    m_iterationCounter(0)
 {
-    assert(m_dHda.size() == in_initialParams.size());
+    // If not provided, use the default propagator (Runge-Kutta) and gradient-based stepper (L-BFGS)
+    if (!m_integrator)
+    {
+        m_integrator = std::make_unique<DefaultIntegrator>(in_maxTime/DEFAULT_NUMBER_STEPS);
+    }
+
+    if (!m_gradStepper)
+    {
+        m_gradStepper =  std::make_unique<DefaultGradientStepper>();
+    }
+}
+
+
+GOAT_PulseOptim::OptimizationResult GOAT_PulseOptim::optimize()
+{
+    m_iterationCounter = 0;
+    double* ptr = &m_initialParams[0];
+    Eigen::Map<Eigen::VectorXd> initParams(ptr, m_initialParams.size());
+    // Invoke the Gradient stepper to drive optimization procedure
+    m_gradStepper->optimize(this, initParams);
+    
+    // Return the result cache (update during optimization run)
+    return m_resultCache;
+}
+
+double GOAT_PulseOptim::operator()(const Eigen::VectorXd& in_params, Eigen::VectorXd& out_grads) 
+{
+    double fx = 0.0;
+    assert(m_dHda.size() == in_params.size());
     // Set-up initial parameters
-    OptimParams params = in_initialParams;
+    OptimParams params;
+    params.resize(in_params.size());
+    Eigen::VectorXd::Map(&params[0], params.size()) = in_params;
+
     // Hamiltonian matrix @ t = 0; params = init
-    const auto initialH = m_hamiltonian(0, in_initialParams);
+    const auto initialH = m_hamiltonian(0, params);
     // Hamiltonian must be a square matrix
     assert(initialH.rows() == initialH.cols());
     const auto dimH = initialH.rows();
     
-    // High level execution
-    while (true)
+    const auto integrateResult = m_integrator->integrate(m_hamiltonian, m_dHda, params, m_maxTime);
+    // We expect the result is a matrix that has (nb_params+1)*dimH rows and dimH columns
+    // i.e. we have U(T) and a vector of dUda(T) (one for each param) stacked on each other.
+    assert(integrateResult.cols() == dimH);
+    assert(integrateResult.rows() == dimH * (1 + params.size()));
+
+    const auto evalCostFn = [&](const Matrix& in_uMat) -> double {
+        assert(m_targetU.rows() == in_uMat.rows() && m_targetU.cols() == in_uMat.cols());
+        // Cost/goal function, i.e. Eq. (4)
+        return 1.0 - (1.0/m_targetU.rows())*std::abs((m_targetU.adjoint() * in_uMat).trace());
+    };
+
+    const auto evalCostFnGradient = [&](const std::vector<Matrix>& in_duMats, const OptimParams& in_params, double in_costVal) -> std::vector<double> {
+        assert(in_duMats.size() == in_params.size());
+        std::vector<double> results;
+
+        for (int i = 0; i < in_params.size(); ++i)
+        {
+            assert(in_duMats[i].rows() == m_targetU.rows() && m_targetU.cols() == in_duMats[i].cols());  
+            // Cost function gradients, i.e., Eq. (6)
+            const auto gradResult = -std::real((1.0/dimH)*(m_targetU.adjoint() * in_duMats[i]).trace());
+            results.emplace_back(gradResult);
+        }
+
+        return results;
+    }; 
+
+    const auto uMat = integrateResult(Eigen::seqN(0, dimH), Eigen::all);
+    std::vector<Matrix> duMats;
+    for (int i = 0; i < params.size(); ++i)
     {
-        // Before the integration
-        const auto integrateResult = m_integrator->integrate(m_hamiltonian, m_dHda, in_maxTime);
-        // We expect the result is a matrix that has (nb_params+1)*dimH rows and dimH columns
-        // i.e. we have U(T) and a vector of dUda(T) (one for each param) stacked on each other.
-        assert(integrateResult.cols() == dimH);
-        assert(integrateResult.rows() == dimH * (1 + params.size()));
+        // Vector of dUda (one for each param)
+        duMats.emplace_back(integrateResult(Eigen::seqN(dimH + i*dimH, dimH), Eigen::all));
+    }
 
-        const auto evalCostFn = [&](const Matrix& in_uMat) -> double {
-            assert(m_targetU.rows() == in_uMat.rows() && m_targetU.cols() == in_uMat.cols());
-            // Const/goal function, i.e. Eq. (4)
-            return 1.0 - (1.0/m_targetU.rows())*std::abs((m_targetU.adjoint() * in_uMat).trace());
-        };
- 
-        const auto evalCostFnGradient = [&](const std::vector<Matrix>& in_duMats, const OptimParams& in_params, double in_costVal) -> std::vector<double> {
-            assert(in_duMats.size() == in_params.size());
-            std::vector<double> results;
+    fx = evalCostFn(uMat);
+    // Evaluate the gradients
+    const std::vector<double> costFnGradVals = evalCostFnGradient(duMats, params, fx);
 
-            for (int i = 0; i < in_params.size(); ++i)
-            {
-                assert(in_duMats[i].rows() == m_targetU.rows() && m_targetU.cols() == in_duMats[i].cols());
-                // Cost function gradients, i.e., Eq. (6)
-                const auto gradResult = - std::real((1/ std::abs(in_costVal))*(1/dimH)*(m_targetU.adjoint() * in_duMats[i]).trace());
-                results.emplace_back(gradResult);
-            }
+    for (int i = 0; i < costFnGradVals.size(); ++i)
+    {
+        out_grads[i] = costFnGradVals[i];
+    }
 
-            return results;
-        }; 
+    // SUMMARY:
+    {
+        std::cout << "        Iteration: " << m_iterationCounter << "\n";
+        std::cout << "Params = " << in_params << "\n";
+        std::cout << "Gradients = " << out_grads << "\n";
+        std::cout << "Cost Function Val = " << fx << "\n";
+        std::cout << "===========================================\n";
+    }
+  
+    {
+        m_resultCache.finalCost = fx;
+        m_resultCache.optParams = params;
+        m_resultCache.nbIters = m_iterationCounter;
+    }
 
-        const auto uMat = integrateResult(Eigen::seqN(0, dimH), Eigen::all);
-        std::vector<Matrix> duMats;
-        for (int i = 0; i < params.size(); ++i)
-        {
-            // Vector of dUda (one for each param)
-            duMats.emplace_back(integrateResult(Eigen::seqN(dimH + i*dimH, dimH), Eigen::all));
-        }
-
-        const double costFnVal = evalCostFn(uMat);
-
-        // Check if we should stop now (e.g. cost function value has reached the target)
-        if (m_stopper->shouldStop(costFnVal, params))
-        {
-            out_finalCost = costFnVal;
-            return params;
-        }
-
-        // If not, continue
-        
-        // Evaluate the gradients
-        const std::vector<double> costFnGradVals = evalCostFnGradient(duMats, params, costFnVal);
-        // Use the provide gradient-based stepper (e.g. BFGS) to find the step sizes
-        const auto paramStepSzs = m_gradStepper->calculate(costFnGradVals);
-        assert(paramStepSzs.size() == params.size());
-
-        for (int i = 0; i < params.size(); ++i)
-        {
-            // Update the params by the step size
-            params[i] += paramStepSzs[i];
-        }
-    } 
+    m_iterationCounter++;
+    return fx;
 }
+
+GOAT_PulseOptim::DefaultIntegrator::DefaultIntegrator(double in_dt):
+    m_dt(in_dt)
+{}
+
+Matrix GOAT_PulseOptim::DefaultIntegrator::integrate(const Hamiltonian& in_hamiltonian, const dHdalpha& in_dHda, const OptimParams& in_params, double in_stopTime)
+{
+    const auto nbParams = in_dHda.size();
+    const auto initialH = in_hamiltonian(0, in_params);
+    const auto dimH = initialH.rows();
+    
+    // initial condition: U(0) = I, dUdalpha(0) = 0
+    Matrix U = Matrix::Identity(dimH, dimH);
+    std::vector<Matrix> dUs;
+    for (int i = 0; i < nbParams; ++i)
+    {
+        dUs.emplace_back(Matrix::Zero(dimH, dimH));
+    }
+   
+    for (int i = 0; i * m_dt < in_stopTime; ++i)
+    {
+        const double currentTime = m_dt*i;
+        const auto currentU = U;
+        
+        std::function<Matrix(float, const Matrix&)> uIntegrateFunc = [&in_hamiltonian, &in_params](float in_time, const Matrix& in_u) -> Matrix {
+            const std::complex<double> I(0.0, 1.0);
+            return -I*in_hamiltonian(in_time, in_params)*in_u;
+        };
+        const RungeKutta uProgagator(uIntegrateFunc);
+
+        std::vector<RungeKutta> dUPropagators;
+        for (const auto& dHda :  in_dHda)
+        {
+            std::function<Matrix(float, const Matrix&)> duIntegrateFunc = [&in_hamiltonian, &in_params, &dHda, &currentU](float in_time, const Matrix& in_u) -> Matrix {
+                const std::complex<double> I(0.0, 1.0);
+                return -I*(dHda(in_time, in_params)*currentU + in_hamiltonian(in_time, in_params)*in_u);
+            };
+            const RungeKutta dUProgagator(duIntegrateFunc);
+
+            dUPropagators.emplace_back(dUProgagator);
+        }
+   
+        // Propagate U 
+        U = uProgagator.step(currentTime, U, m_dt);
+        // Propagate dUdalpha (params)
+        for (int j = 0; j < nbParams; ++j)
+        {
+           dUs[j] = dUPropagators[j].step(currentTime, dUs[j], m_dt);
+        }
+    }
+   
+    Matrix result = U;     
+    for (const auto& dUMat : dUs)
+    {
+        result = vstack(result, dUMat);
+    }
+
+    return result;
+}
+
+void GOAT_PulseOptim::DefaultGradientStepper::optimize(IOptimizationFunction* io_problem, const Eigen::VectorXd& in_initialParams) 
+{
+    // Set up parameters
+    // TODO: enable customization via Heterogenous map
+    // We are using a very simple L-BFGS implementation (~100 lines of code)
+    // Interestingly enough, both ML-PACK and NL-OPT were *UNABLE* to converge
+    // but this simple impl can converge just fine.
+    // Looks like ML-PACK and NL-OPT were stuck at local minima and don't have any mechanisms to escape.
+    // For now, just use this simple implementation.
+    LBFGSpp::LBFGSParam<double> param;
+    param.epsilon = 1e-6;
+    param.max_iterations = 50;
+    LBFGSpp::LBFGSSolver<double> solver(param);
+    
+    double fx = 0.0;
+    Eigen::VectorXd x = in_initialParams;
+    solver.minimize(*io_problem, x, fx);
+}
+    
---
+++ quantum/plugins/optimal_control/tests/GOATTester.cpp
@@ -0,0 +1,122 @@
+#include <gtest/gtest.h>
+#include "xacc.hpp"
+#include <string>
+#include "GOAT.hpp"
+#include "exprtk.hpp"
+
+using symbol_table_t = exprtk::symbol_table<double>;
+using expression_t = exprtk::expression<double>;
+using parser_t = exprtk::parser<double>;
+
+TEST(GOATTester, testSimple)
+{
+    // Simple test:
+    // Drive term is a gaussian pulse (analytical form) 
+    // Optimization param is sigma (1 param only)
+    // d(t) = exp(-t^2/2*sigma^2)
+    const Matrix iden = Matrix::Identity(2, 2);
+    Matrix pauliX{ Matrix::Zero(2, 2) };
+    Matrix pauliZ{ Matrix::Zero(2, 2) };
+    const double tMax = 100;
+    const double omega_a = 2*M_PI/tMax;
+    pauliX << 0, 1, 1, 0;
+    pauliZ << 1, 0, 0, -1;
+
+    // Hamiltonian: specified as a function of params and time
+    const std::string hamiltonianFn = "exp(-t^2/(2*sigma^2))";
+    expression_t expression;
+    parser_t parser;
+    symbol_table_t symbol_table;
+    // Placeholders for variable names
+    double g_time = 0.0;
+    double g_sigma = 0.0;
+    symbol_table.add_variable("t", g_time);
+    symbol_table.add_variable("sigma", g_sigma);
+    expression.register_symbol_table(symbol_table);
+    parser.compile(hamiltonianFn, expression);
+   
+    const Hamiltonian ham = [&](double in_time, OptimParams in_params) -> Matrix {
+        assert(in_params.size() == 1);
+       
+        // We only has 1 parameter which is the gamma.
+        double sigma = in_params[0];
+        // Set the variables for evaluation. 
+        g_time = in_time;
+        g_sigma = sigma;
+        // Evaluation result by ExprTk
+        const auto exprResult = omega_a*expression.value(); 
+        // Analytical result:
+        const auto pulseVal = omega_a*std::exp(-std::pow(in_time, 2) / (2 * std::pow(sigma, 2)));
+        // Verify that the evaluation (string-based expressions)
+        EXPECT_NEAR(exprResult, pulseVal, 1e-6);    
+        
+        return exprResult * pauliX;
+    };
+
+    // Differential of H w.r.t. sigma parameter
+    const Hamiltonian dHdsigma = [&](double in_time, OptimParams in_params) -> Matrix {
+        assert(in_params.size() == 1);
+        const double sigma = in_params[0];
+        // Derivative (analytical)      
+        const double expectedDerivVal = omega_a * (std::pow(in_time, 2)/ std::pow(sigma, 3)) * std::exp(-std::pow(in_time, 2) / (2 * std::pow(sigma, 2)));
+        
+        // Derivative w.r.t. the sigma parameter
+        g_time = in_time;
+        g_sigma = sigma;
+        // Compute the derivative (exprtk)
+        const double exprDerivEval = omega_a * exprtk::derivative(expression, "sigma");
+        
+        // Verify that the derivative evaluation (string-based expressions)
+        EXPECT_NEAR(exprDerivEval, expectedDerivVal, 1e-6);    
+        return exprDerivEval * pauliX;
+    };
+
+    const dHdalpha dHdparams { dHdsigma };
+    // Let's say we want to optimize an X gate: U target = Pauli X
+    const std::complex<double> I(0.0, 1.0);
+    // Note: this Hamiltonian will result in a global phase of 1i
+    // hence, the target U must be set to iX,
+    // otherwise the cost functional and gradients will not work.
+    const Matrix targetU = I * pauliX;
+
+    // Expected answer
+    const double EXPECTED_OPTIMAL_GAMMA = 20.0; 
+
+    const auto runTest = [&](double in_initialValue){
+        OptimParams initialParam { in_initialValue };
+
+        // Construct the GOAT optimizer
+        GOAT_PulseOptim myOptimizer(targetU, ham, dHdparams, initialParam, tMax);
+        const auto optResult = myOptimizer.optimize();
+        
+        EXPECT_NEAR(optResult.finalCost, 0.0, 1e-3);
+        // We only have 1 parameter (gamma)
+        EXPECT_EQ(optResult.optParams.size(), 1);
+        // Within 10% variation 
+        EXPECT_NEAR(optResult.optParams[0], EXPECTED_OPTIMAL_GAMMA, 0.1*EXPECTED_OPTIMAL_GAMMA);
+        // We cap the number of iterations to 50 in GOAT,
+        // check that indeed we can converge, not terminate early.
+        EXPECT_LT(optResult.nbIters, 50);
+
+        std::cout << "Found OPTIMAL gamma = " << optResult.optParams[0] << " after " << optResult.nbIters << " iterations. \n";
+    };
+
+    // Test 1: Initial pulse width = 8ns (below the optimal value ~20)
+    std::cout << "##########################################\n";
+    runTest(8.0);
+    std::cout << "##########################################\n";
+
+    // Test2: Initial pulse width = 30ns (above the optimal value ~20)
+    std::cout << "##########################################\n";
+    runTest(30.0);
+    std::cout << "##########################################\n";
+}
+
+int main(int argc, char **argv) 
+{
+    xacc::Initialize();
+    ::testing::InitGoogleTest(&argc, argv);
+    const auto result = RUN_ALL_TESTS();
+    xacc::Finalize();
+    return result;
+}
