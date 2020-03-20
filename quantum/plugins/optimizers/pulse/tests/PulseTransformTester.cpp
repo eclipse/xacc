2d2a163c0d6b335cee2e134238e8ab001a643b0e
Thien Nguyen <nguyentm@ornl.gov>
2020-03-20 09:50:04 -0400

    Some code refactor for gate fusion and unit test improvement
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- quantum/plugins/optimizers/pulse/tests/PulseTransformTester.cpp
+++ quantum/plugins/optimizers/pulse/tests/PulseTransformTester.cpp
@@ -2,6 +2,9 @@
 #include "xacc.hpp"
 #include "xacc_service.hpp"
 #include "GateFusion.hpp"
+namespace {
+    const std::complex<double> I(0.0, 1.0);
+}
 
 TEST(PulseTransformTester, checkSimple) 
 {
@@ -53,6 +56,30 @@ TEST(PulseTransformTester, checkSimple)
         std::cout << result << "\n";
         // TODO: add checks
     }
+
+    // Two qubit gate with non-continuous index
+    // CNOT b/w q[0] and q[2] 
+    {
+        auto program = compiler->compile(
+            R"(__qpu__ void test3(qbit q) {
+                CNOT(q[0], q[2]);
+            })")->getComposites()[0];
+        auto fuser = xacc::getService<GateFuser>("default");
+        fuser->initialize(program);
+        auto result = fuser->calcFusedGate(3);
+        // Expected location (row, column) that has an element 1.0
+        // Q0 is the control, hence it is a map 4(100)->5(101), 6->7 and vice versa
+        std::vector<std::pair<size_t, size_t>> expectedLoc {
+            {0, 0}, {1, 1}, {2, 2}, {3, 3}, {4, 5}, { 5, 4}, {6, 7}, {7, 6}
+        };
+        // std::cout << result << "\n";
+        for (const auto& loc : expectedLoc)
+        {
+            // These entries are 1.0
+            EXPECT_NEAR(result(loc.first, loc.second).real(), 1.0, 1e-12);
+        }
+    }
+
     // QFT circuit
     {
         auto tmp = xacc::getService<Instruction>("qft");
@@ -63,7 +90,17 @@ TEST(PulseTransformTester, checkSimple)
         fuser->initialize(qft);
         auto result = fuser->calcFusedGate(2);
         std::cout << "QFT Matrix: \n" << result << "\n";
-        // TODO: add checks
+        // Expected matrix
+        Eigen::MatrixXcd QftMat{ Eigen::MatrixXcd::Zero(4, 4) }; 
+        QftMat <<   1, 1, 1, 1,
+                    1, I, -1, -I,
+                    1, -1, 1, -1,
+                    1, -I, -1, I;
+        QftMat *= 0.5;
+        std::cout << "Expected QFT Matrix: \n" << result << "\n";
+        // Compare the result matrix vs. expected.
+        const auto diff = (result - QftMat).norm();
+        EXPECT_NEAR(diff, 0.0, 1e-6);
     }
 }
 
