9d4e44854850ee1631346074db9818df1f62900c
Thien Nguyen <nguyentm@ornl.gov>
2020-03-24 13:49:22 -0400

    Update pulse IR transformation to handle GRAPE
    At IR transformation interface level, the only difference is that GRAPE doesn't require analytical function inputs.
    Hence, just need to scope those analytical param parsing into an if block (GOAT only).
    
    At the other end, once the optimization procedure is completed, for GRAPE, we just need to use the data array to construct pulse instructions (no need to evaluate the functions as for GOAT).
    
    Tested by: unit testing in QuaC where we use this IR transformation service.

--- quantum/plugins/optimizers/pulse/PulseTransform.cpp
+++ quantum/plugins/optimizers/pulse/PulseTransform.cpp
@@ -106,36 +106,43 @@ namespace quantum {
             return xacc::error("Missing required field 'method'");
         }
 
+        // These parameters are only required for analytical controls (e.g. GOAT)
         std::vector<std::string> controlParams;    
-        if (options.keyExists<std::vector<std::string>>("control-params")) 
-        {
-            controlParams = options.get<std::vector<std::string>>("control-params");
-        }
-        else
-        {
-            xacc::error("Missing required field 'control-params'");
-        }
-
         std::vector<std::string> controlFuncs;    
-        if (options.keyExists<std::vector<std::string>>("control-funcs")) 
-        {
-            controlFuncs = options.get<std::vector<std::string>>("control-funcs");
-        }
-        else
-        {
-            xacc::error("Missing required field 'control-funcs'");
-        }
-
         std::vector<double> initParams;    
-        if (options.keyExists<std::vector<double>>("initial-parameters")) 
-        {
-            initParams = options.get<std::vector<double>>("initial-parameters");
-        }
-        else
-        {
-            xacc::error("Missing required field 'initial-parameters'");
-        }
 
+        if (method == "GOAT")
+        {
+            if (options.keyExists<std::vector<std::string>>("control-params")) 
+            {
+                controlParams = options.get<std::vector<std::string>>("control-params");
+            }
+            else
+            {
+                xacc::error("Missing required field 'control-params'");
+            }
+
+            if (options.keyExists<std::vector<std::string>>("control-funcs")) 
+            {
+                controlFuncs = options.get<std::vector<std::string>>("control-funcs");
+            }
+            else
+            {
+                xacc::error("Missing required field 'control-funcs'");
+            }
+
+            if (options.keyExists<std::vector<double>>("initial-parameters")) 
+            {
+                initParams = options.get<std::vector<double>>("initial-parameters");
+            }
+            else
+            {
+                xacc::error("Missing required field 'initial-parameters'");
+            }
+
+            assert(controlOps.size() == controlFuncs.size());
+        }
+        
         double tMax = 0.0;
         if (options.keyExists<double>("max-time")) 
         {
@@ -145,8 +152,6 @@ namespace quantum {
         {
             return xacc::error("Missing required field 'max-time'");
         }
-
-        assert(controlOps.size() == controlFuncs.size());
 
         std::cout << "Perform pulse optimization:\n";
         
@@ -169,70 +174,114 @@ namespace quantum {
             std::make_pair("dimension", dim),
             // Target unitary: the one calculated from the CompositeInstruction
             std::make_pair("target-U", flattenUMat),
+            // Analytical control parameters
+            // Note: these params are only required by GOAT
+            // we just send on empty params in other cases. 
             // Control parameter (used in the control function)
             std::make_pair("control-params", controlParams),
             // Control funcs
             std::make_pair("control-funcs", controlFuncs),
+            // Initial params
+            std::make_pair("initial-parameters", initParams),
+            //////////////////////////////////////////////////
             // Control Hamiltonian terms: i.e. channels
             std::make_pair("control-H", controlOps),
             // Static Hamiltonian
             std::make_pair("static-H", H0),
-            // Initial params
-            std::make_pair("initial-parameters", initParams),
             // Max time
-            std::make_pair("max-time", tMax)
+            std::make_pair("max-time", tMax),
+            std::make_pair("dt", backendDt)
         };
 
         auto optimizer = xacc::getOptimizer("quantum-control", pulseOptimConfigs);
         // Perform pulse IR transformation
         const auto optimResult = optimizer->optimize();
         
-        // Step 5: construct the *analytical* form of optimal control functions
-        // i.e. replace each control-params variable with its optimal value
-        std::vector<std::string> resolvedFuncs;
+        // If analytical control (e.g. GOAT), generate pulses based on optimized analytical form.
+        if (method == "GOAT")
+        {
+            // Step 5: construct the *analytical* form of optimal control functions
+            // i.e. replace each control-params variable with its optimal value
+            std::vector<std::string> resolvedFuncs;
             
-        const auto replaceAllVarOccurrences = [](const std::string& in_func, 
-                                                    const std::string& in_varName, 
-                                                    double in_varVal)->std::string {
-            size_t pos = 0;
-            std::string resultStr = in_func;
-            while ((pos = resultStr.find(in_varName, pos)) != std::string::npos) 
-            {
-                const std::string replace = std::to_string(in_varVal);
-                resultStr.replace(pos, in_varName.length(), replace);
-                pos += replace.length();
-            }
-            return resultStr;
-        };
+            const auto replaceAllVarOccurrences = [](const std::string& in_func, 
+                                                        const std::string& in_varName, 
+                                                        double in_varVal)->std::string {
+                size_t pos = 0;
+                std::string resultStr = in_func;
+                while ((pos = resultStr.find(in_varName, pos)) != std::string::npos) 
+                {
+                    const std::string replace = std::to_string(in_varVal);
+                    resultStr.replace(pos, in_varName.length(), replace);
+                    pos += replace.length();
+                }
+                return resultStr;
+            };
 
-        for (const auto& ctrlFn : controlFuncs)
-        {
-            assert(optimResult.second.size() == controlParams.size());
-            std::string evaledFunc = ctrlFn;
-            // Replace all variables
-            for (size_t i = 0; i < controlParams.size(); ++i)
+            for (const auto& ctrlFn : controlFuncs)
             {
-                evaledFunc = replaceAllVarOccurrences(evaledFunc, controlParams[i], optimResult.second[i]);
+                assert(optimResult.second.size() == controlParams.size());
+                std::string evaledFunc = ctrlFn;
+                // Replace all variables
+                for (size_t i = 0; i < controlParams.size(); ++i)
+                {
+                    evaledFunc = replaceAllVarOccurrences(evaledFunc, controlParams[i], optimResult.second[i]);
+                }
+
+                std::cout << "Evaled control function: " << evaledFunc << "\n";
+                resolvedFuncs.emplace_back(evaledFunc);
             }
 
-            std::cout << "Evaled control function: " << evaledFunc << "\n";
-            resolvedFuncs.emplace_back(evaledFunc);
+            // Step 6: Transform the input composite
+            // Clear all instructions (gates)
+            program->clear();
+            int id = 0;
+            // Add optimized pulses
+            for (const auto& pulseFn : resolvedFuncs)
+            {
+                std::cout << "Add pulse function: " << pulseFn << "\n";
+                const std::string pulse_name = "Optim_Pulse_" + std::to_string(id);          
+                auto pulse = std::make_shared<xacc::quantum::Pulse>(pulse_name);
+                pulse->setChannel(controlChannels[id]);
+                pulse->setSamples(pulseFunc(pulseFn, tMax, backendDt));
+                program->addInstruction(pulse);
+                ++id;
+            }
         }
-
-        // Step 6: Transform the input composite
-        // Clear all instructions (gates)
-        program->clear();
-        int id = 0;
-        // Add optimized pulses
-        for (const auto& pulseFn : resolvedFuncs)
+        // If "GRAPE" (or other time-series methods),
+        // we already optimize the pulses as data arrays, 
+        // hence just need to make pulses from those arrays.
+        if (method == "GRAPE")
         {
-            std::cout << "Add pulse function: " << pulseFn << "\n";
-            const std::string pulse_name = "Optim_Pulse_" + std::to_string(id);          
-            auto pulse = std::make_shared<xacc::quantum::Pulse>(pulse_name);
-            pulse->setChannel(controlChannels[id]);
-            pulse->setSamples(pulseFunc(pulseFn, tMax, backendDt));
-            program->addInstruction(pulse);
-            ++id;
+            program->clear();
+            const auto nbControls = controlOps.size();
+            if (optimResult.second.size() % nbControls != 0)
+            {
+                xacc::error("FATAL: Invalid results from pulse optimizer!");
+                return;
+            }
+            // We have flattened the pulses into one array, now de-mux into pulses.
+            const int pulseLength = optimResult.second.size()/nbControls;
+            // Add optimized pulses
+            for (int pulseId = 0; pulseId < nbControls; ++pulseId)
+            {
+                const std::string pulse_name = "Grape_Optim_Pulse_" + std::to_string(pulseId);          
+                auto pulse = std::make_shared<xacc::quantum::Pulse>(pulse_name);
+                pulse->setChannel(controlChannels[pulseId]);
+                
+                std::vector<std::vector<double>> samples;
+                samples.reserve(pulseLength);
+
+                for (int i = 0; i < pulseLength; ++i)
+                {
+                    const auto idx = i + pulseId*pulseLength;
+                    const std::vector<double> sample { optimResult.second[idx] };
+                    samples.emplace_back(sample);
+                }
+
+                pulse->setSamples(samples);
+                program->addInstruction(pulse);
+            }
         }
 
         // Debug:
