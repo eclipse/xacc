f71321e00abba8ee33554972f1bdbf700e16aac6
Thien Nguyen <nguyentm@ornl.gov>
2020-05-12 14:06:32 -0400

    Added Cppmicroservices boilerplate for QAOA algo
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

---
+++ quantum/plugins/algorithms/qaoa/qaoa.hpp
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2019 UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompanies this
+ * distribution. The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html and the Eclipse Distribution
+ *License is available at https://eclipse.org/org/documents/edl-v10.php
+ *
+ * Contributors:
+ *   Thien Nguyen - initial API and implementation
+ *******************************************************************************/
+#pragma once
+
+#include "Algorithm.hpp"
+
+namespace xacc {
+namespace algorithm {
+class QAOA : public Algorithm 
+{
+public:
+    bool initialize(const HeterogeneousMap& parameters) override;
+    const std::vector<std::string> requiredParameters() const override;
+    void execute(const std::shared_ptr<AcceleratorBuffer> buffer) const override;
+    std::vector<double> execute(const std::shared_ptr<AcceleratorBuffer> buffer, const std::vector<double> &parameters) override;
+    const std::string name() const override { return "QAOA"; }
+    const std::string description() const override { return ""; }
+    DEFINE_ALGORITHM_CLONE(QAOA)
+};
+} // namespace algorithm
+} // namespace xacc
