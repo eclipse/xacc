1af7f1854a149345d884e714d446cee01ee59859
Thien Nguyen <nguyentm@ornl.gov>
2020-06-19 08:58:30 -0400

    Microservices boilerplate
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

---
+++ quantum/plugins/circuits/qfast/tests/QfastTester.cpp
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2019 UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompanies this
+ * distribution. The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html and the Eclipse Distribution
+ *License is available at https://eclipse.org/org/documents/edl-v10.php
+ *
+ * Contributors:
+ *   Thien Nguyen - initial API and implementation
+ *******************************************************************************/
+
+#include "xacc.hpp"
+#include <gtest/gtest.h>
+#include "Circuit.hpp"
+#include "Optimizer.hpp"
+#include "xacc_observable.hpp"
+#include "xacc_service.hpp"
+
+using namespace xacc;
+
+TEST(QFastTester, checkSimple) 
+{
+   
+}
+
+
+int main(int argc, char **argv) {
+    xacc::Initialize(argc, argv);
+    ::testing::InitGoogleTest(&argc, argv);
+    auto ret = RUN_ALL_TESTS();
+    xacc::Finalize();
+    return ret;
+}
