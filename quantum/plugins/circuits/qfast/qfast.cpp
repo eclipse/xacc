c00de4bb851953497cfc16a3d12d1946de7fbe08
Thien Nguyen <nguyentm@ornl.gov>
2020-06-19 14:52:11 -0400

    Added some Pauli basis permutation helpers
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- quantum/plugins/circuits/qfast/qfast.cpp
+++ quantum/plugins/circuits/qfast/qfast.cpp
@@ -14,6 +14,68 @@
 #include "qfast.hpp"
 #include "xacc.hpp"
 #include "xacc_service.hpp"
+#include <unsupported/Eigen/KroneckerProduct>
+
+namespace {
+Eigen::MatrixXcd X { Eigen::MatrixXcd::Zero(2, 2)};      
+Eigen::MatrixXcd Y { Eigen::MatrixXcd::Zero(2, 2)};       
+Eigen::MatrixXcd Z { Eigen::MatrixXcd::Zero(2, 2)};  
+Eigen::MatrixXcd Id { Eigen::MatrixXcd::Identity(2, 2)};  
+
+constexpr std::complex<double> I { 0.0, 1.0 };
+const std::vector<Eigen::MatrixXcd>& getPaulisMap(int n)
+{
+    static std::vector<std::vector<Eigen::MatrixXcd>> cachedMap;
+    static bool init = false;
+    if (!init)
+    {
+        X << 0, 1, 1, 0;
+        Y << 0, -I, I, 0;
+        Z << 1, 0, 0, -1;
+        cachedMap.emplace_back(std::vector<Eigen::MatrixXcd> { Id });
+        cachedMap.emplace_back(std::vector<Eigen::MatrixXcd> { Id, X, Y, Z });
+        init = true;
+    }
+
+    if (n < cachedMap.size())
+    {
+        return cachedMap[n];
+    }
+
+    std::vector<Eigen::MatrixXcd> norderPaulis;
+    
+    for (const auto& paulisNminus1 : getPaulisMap(n - 1))
+    {
+        for (const auto& paulis1 : getPaulisMap(1))
+        {
+            norderPaulis.emplace_back(Eigen::kroneckerProduct(paulisNminus1, paulis1));
+        }
+    }
+    
+    cachedMap.emplace_back(std::move(norderPaulis));
+    return cachedMap[n];
+}
+
+enum class PauliOp : size_t { I = 0, X = 1, Y = 2, Z = 3 };
+const PauliOp SinglePauliOps[] = { PauliOp::I, PauliOp::X, PauliOp::Y, PauliOp::Z };
+
+// Index to the all Paulis (at a specific number of qubits):
+// Note: we limit ourselves to the case of two operators only.
+// Indexing scheme: Pauli string as base-4 bitstring:
+// with I = 0, X = 1, Y = 2, Z = 3
+// e.g. XXY = 1 * 4^2 + 1 & 4^1 + 2 * 4^0 = 22 (in base 10)
+size_t computePauliMapIndex(size_t in_nbQubits, const std::pair<PauliOp, size_t>& in_op1, const std::pair<PauliOp, size_t>& in_op2)
+{
+    assert(in_op1.second != in_op2.second);
+    assert(in_op1.second < in_nbQubits);
+    assert(in_op2.second < in_nbQubits);
+    const auto opToIndx = [&in_nbQubits](const std::pair<PauliOp, size_t>& in_op) -> size_t {
+        return static_cast<size_t>(in_op.first) * std::pow(4, in_nbQubits - in_op.second - 1);
+    };
+    return opToIndx(in_op1) + opToIndx(in_op2);
+}
+
+}
 
 namespace xacc {
 namespace circuits {
@@ -23,6 +85,14 @@ QFAST::QFAST():
 
 bool QFAST::expand(const xacc::HeterogeneousMap& runtimeOptions) 
 {
+    // !! TEMP CODE !!
+    m_nbQubits = 3;
+    m_topology = { { 0, 1}, { 1, 2} };
+    // !! TEMP CODE !!
+
+    // Generate and cache the Pauli decompose basis:
+    m_allPaulis = generateAllPaulis(m_nbQubits, m_topology);
+    
     // TODO
     return true;
 }
@@ -56,6 +126,33 @@ std::shared_ptr<CompositeInstruction> QFAST::genericBlockToGates(const QFAST::Bl
 {
     // TODO
     return nullptr;
+}
+ 
+std::vector<Eigen::MatrixXcd> QFAST::generateAllPaulis(size_t in_nbQubits, const Topology& in_topology)
+{
+    // Minimum: 3 qubits
+    assert(in_nbQubits > 2);
+    // Must have at least 2 pairs of connected qubits.
+    assert(in_topology.size() > 1);
+    // Get all Paulis
+    const auto& paulis = getPaulisMap(in_nbQubits);
+
+    std::vector<Eigen::MatrixXcd> result;
+    
+    for (const auto& pair : in_topology)
+    {
+        const auto bit1 = pair.first;
+        const auto bit2 = pair.second;
+        for (const auto& op1 : SinglePauliOps)
+        {
+            for (const auto& op2 : SinglePauliOps)
+            {
+                result.emplace_back(paulis[computePauliMapIndex(in_nbQubits, { op1, bit1 }, { op2, bit2 })]);
+            }
+        }
+    }
+
+    return result;
 }
 }
 }
