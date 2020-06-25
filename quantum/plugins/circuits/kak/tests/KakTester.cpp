e6a7f2524708df10b31878ef46789a9e70d6abe6
Thien Nguyen <nguyentm@ornl.gov>
2020-06-25 14:16:02 -0400

    Added KAK gate instantiation using exp_i_theta
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- quantum/plugins/circuits/kak/tests/KakTester.cpp
+++ quantum/plugins/circuits/kak/tests/KakTester.cpp
@@ -33,6 +33,35 @@ TEST(KakTester, checkSimple)
   }
 }
 
+// Decompose CNOT
+TEST(KakTester, checkCNOT) 
+{
+  Eigen::Matrix4cd cnotMat;
+  cnotMat << 1, 0, 0, 0,
+             0, 1, 0, 0,
+             0, 0, 0, 1,
+             0, 0, 1, 0;
+  
+  auto tmp = xacc::getService<Instruction>("kak");
+  auto kak = std::dynamic_pointer_cast<quantum::Circuit>(tmp);
+  const bool expandOk = kak->expand({ 
+    std::make_pair("unitary", cnotMat)
+  });
+  EXPECT_TRUE(expandOk);
+  auto acc = xacc::getAccelerator("qpp", { std::make_pair("shots", 1024)});
+  auto gateRegistry = xacc::getService<IRProvider>("quantum");
+  auto xGate0 = gateRegistry->createInstruction("X", { 0 });
+  auto xGate1 = gateRegistry->createInstruction("X", { 1 });
+  auto measureGate0 = gateRegistry->createInstruction("Measure", { 0 });
+  auto measureGate1 = gateRegistry->createInstruction("Measure", { 1 });
+  //kak->insertInstruction(0, xGate0);
+  kak->insertInstruction(0, xGate1);
+  kak->addInstructions({ measureGate0, measureGate1 });
+  std::cout << "HOWDY: \n" << kak->toString() << "\n";
+  auto buffer = xacc::qalloc(2);
+  acc->execute(buffer, kak);
+  buffer->print();
+}
 int main(int argc, char **argv) {
   xacc::Initialize(argc, argv);
   ::testing::InitGoogleTest(&argc, argv);
