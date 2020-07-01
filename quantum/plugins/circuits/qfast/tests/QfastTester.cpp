5207f0b4c53360b6b8f911a1d8a3e7b500c6f6d5
Thien Nguyen <nguyentm@ornl.gov>
2020-07-01 11:26:01 -0400

    Restored the QFast unit test and added docs
    The bug has now been fixed.
    
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- quantum/plugins/circuits/qfast/tests/QfastTester.cpp
+++ quantum/plugins/circuits/qfast/tests/QfastTester.cpp
@@ -22,96 +22,82 @@ using namespace xacc;
 
 TEST(QFastTester, checkSimple) 
 {
-  // Re-try max 5 times:
-  for (int j = 0; j < 5; ++j)
+  auto tmp = xacc::getService<Instruction>("QFAST");
+  auto qfast = std::dynamic_pointer_cast<quantum::Circuit>(tmp);
+  Eigen::MatrixXcd ccnotMat = Eigen::MatrixXcd::Identity(8, 8);
+  ccnotMat(6, 6) = 0.0;
+  ccnotMat(7, 7) = 0.0;
+  ccnotMat(6, 7) = 1.0;
+  ccnotMat(7, 6) = 1.0;
+  
+  const bool expandOk = qfast->expand({ 
+    std::make_pair("unitary", ccnotMat)
+  });
+  EXPECT_TRUE(expandOk);
+  
+  auto acc = xacc::getAccelerator("qpp", { std::make_pair("shots", 8192)});
+  auto gateRegistry = xacc::getService<IRProvider>("quantum");
+  auto xGate0 = gateRegistry->createInstruction("X", { 0 });
+  auto xGate1 = gateRegistry->createInstruction("X", { 1 });
+  auto xGate2 = gateRegistry->createInstruction("X", { 2 });
+  auto measureGate0 = gateRegistry->createInstruction("Measure", { 0 });
+  auto measureGate1 = gateRegistry->createInstruction("Measure", { 1 });
+  auto measureGate2 = gateRegistry->createInstruction("Measure", { 2 });
+
+  const auto runTestCase = [&](bool in_bit0, bool in_bit1, bool in_bit2) {
+    static int counter = 0;
+    auto composite = gateRegistry->createComposite("__TEMP_COMPOSITE__" + std::to_string(counter));
+    counter++;
+    // State prep:
+    if (in_bit0)
+    {
+      composite->addInstruction(xGate0);
+    }
+    if (in_bit1)
+    {
+      composite->addInstruction(xGate1);
+    }
+    if (in_bit2)
+    {
+      composite->addInstruction(xGate2);
+    }
+
+    std::string inputBitString;
+    inputBitString.append(in_bit0 ? "1" : "0");
+    inputBitString.append(in_bit1 ? "1" : "0");
+    inputBitString.append(in_bit2 ? "1" : "0");
+
+    composite->addInstructions(qfast->getInstructions());
+    // Mesurement:
+    composite->addInstructions({ measureGate0, measureGate1, measureGate2 });
+
+    auto buffer = xacc::qalloc(3);
+    acc->execute(buffer, composite);
+    std::cout << "Input bitstring: " << inputBitString << "\n";
+    buffer->print();
+    // CCNOT gate:
+    const auto expectedBitString = [&inputBitString]() -> std::string {
+      // If both control bits are 1's
+      if (inputBitString == "110")
+      {
+        return "111";
+      }
+      if (inputBitString == "111")
+      {
+        return "110";
+      }
+      // Otherwise, no changes
+      return inputBitString;
+    }();
+
+    // Check bit string
+    EXPECT_NEAR(buffer->computeMeasurementProbability(expectedBitString),  1.0,  0.1);
+  };  
+
+  // 3 bits: run all test cases (8)
+  for (int i = 0; i < (1 << 3); ++i)
   {
-    auto tmp = xacc::getService<Instruction>("QFAST");
-    auto qfast = std::dynamic_pointer_cast<quantum::Circuit>(tmp);
-    Eigen::MatrixXcd ccnotMat = Eigen::MatrixXcd::Identity(8, 8);
-    ccnotMat(6, 6) = 0.0;
-    ccnotMat(7, 7) = 0.0;
-    ccnotMat(6, 7) = 1.0;
-    ccnotMat(7, 6) = 1.0;
-    
-    const bool expandOk = qfast->expand({ 
-      std::make_pair("unitary", ccnotMat)
-    });
-    EXPECT_TRUE(expandOk);
-    
-    auto acc = xacc::getAccelerator("qpp", { std::make_pair("shots", 8192)});
-    auto gateRegistry = xacc::getService<IRProvider>("quantum");
-    auto xGate0 = gateRegistry->createInstruction("X", { 0 });
-    auto xGate1 = gateRegistry->createInstruction("X", { 1 });
-    auto xGate2 = gateRegistry->createInstruction("X", { 2 });
-    auto measureGate0 = gateRegistry->createInstruction("Measure", { 0 });
-    auto measureGate1 = gateRegistry->createInstruction("Measure", { 1 });
-    auto measureGate2 = gateRegistry->createInstruction("Measure", { 2 });
-
-    const auto runTestCase = [&](bool in_bit0, bool in_bit1, bool in_bit2) {
-      static int counter = 0;
-      auto composite = gateRegistry->createComposite("__TEMP_COMPOSITE__" + std::to_string(counter));
-      counter++;
-      // State prep:
-      if (in_bit0)
-      {
-        composite->addInstruction(xGate0);
-      }
-      if (in_bit1)
-      {
-        composite->addInstruction(xGate1);
-      }
-      if (in_bit2)
-      {
-        composite->addInstruction(xGate2);
-      }
-
-      std::string inputBitString;
-      inputBitString.append(in_bit0 ? "1" : "0");
-      inputBitString.append(in_bit1 ? "1" : "0");
-      inputBitString.append(in_bit2 ? "1" : "0");
-
-      composite->addInstructions(qfast->getInstructions());
-      // Mesurement:
-      composite->addInstructions({ measureGate0, measureGate1, measureGate2 });
-
-      auto buffer = xacc::qalloc(3);
-      acc->execute(buffer, composite);
-      std::cout << "Input bitstring: " << inputBitString << "\n";
-      buffer->print();
-      // CCNOT gate:
-      const auto expectedBitString = [&inputBitString]() -> std::string {
-        // If both control bits are 1's
-        if (inputBitString == "110")
-        {
-          return "111";
-        }
-        if (inputBitString == "111")
-        {
-          return "110";
-        }
-        // Otherwise, no changes
-        return inputBitString;
-      }();
-      // Check bit string
-      const bool result = std::abs(buffer->computeMeasurementProbability(expectedBitString) - 1.0) < 0.1;
-      return result;
-    };
-    
-    // 3 bits: run all test cases
-    bool allSuccess = true;
-    for (int i = 0; i < (1 << 3); ++i)
-    {
-      allSuccess = allSuccess && runTestCase(i & 0x001, i & 0x002, i & 0x004);
-    }
-    if (allSuccess)
-    {
-      std::cout << "Succeeded after " << j << " trials\n";
-      break;
-    }
-    else
-    {
-      std::cout << "Retry\n";
-    }
+    runTestCase(i & 0x001, i & 0x002, i & 0x004);
   }
 }
 
