8a04bb530ebd0b86677b6d72c5e52d5ac8a70527
Thien Nguyen <nguyentm@ornl.gov>
2020-08-25 15:57:41 -0400

    Complete TriQ circuit construction
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- quantum/plugins/placement/triq/triq_placement.cpp
+++ quantum/plugins/placement/triq/triq_placement.cpp
@@ -15,6 +15,26 @@
 #include "xacc_service.hpp"
 #include "circuit.hpp"
 
+namespace {
+std::string xaccGateToTriqGate(const std::string &in_xaccGateName) {
+  if (in_xaccGateName == "Measure") {
+    return "MeasZ";
+  } else if (in_xaccGateName == "Tdg") {
+    return "Tdag";
+  } else if (in_xaccGateName == "Sdg") {
+    return "Sdag";
+  } else if (in_xaccGateName == "Rx") {
+    return "RX";
+  } else if (in_xaccGateName == "Ry") {
+    return "RY";
+  } else if (in_xaccGateName == "Rz") {
+    return "RZ";
+  } else if (in_xaccGateName == "Swap") {
+    return "SWAP";
+  }
+  return in_xaccGateName;
+}
+} // namespace
 namespace xacc {
 namespace quantum {
 
@@ -34,15 +54,37 @@ void TriQPlacement::apply(std::shared_ptr<CompositeInstruction> function,
   for (size_t instIdx = 0; instIdx < function->nInstructions(); ++instIdx) {
     auto xaccInst = function->getInstruction(instIdx);
     const std::string gateName = xaccInst->name();
-    Gate *pG = Gate::create_new_gate(gateName);
+    Gate *pG = Gate::create_new_gate(xaccGateToTriqGate(gateName));
     pG->id = instIdx;
-    const auto nqVars = xaccInst->bits().size();
-    assert(nqVars == pG->nvars);
-
-    for (int j = 0; j < pG->nvars; j++) {
-      pG->vars.push_back(triqCirc.qubits[xaccInst->bits()[j]]);
+    // Make sure the expected number of qubits matched for each gate
+    assert(xaccInst->bits().size() == pG->nvars);
+    for (const auto& qubitIdx : xaccInst->bits()) {
+      pG->vars.push_back(triqCirc.qubits[qubitIdx]);
     }
     triqCirc.gates.push_back(pG);
+  }
+  // Build DAG info
+  auto graphView = function->toGraph();
+  for (size_t i = 1; i < graphView->order() - 1; ++i) {
+    auto node = graphView->getVertexProperties(i);
+    const auto nodeName = node.getString("name");
+    const auto nodeId = node.get<std::size_t>("id");
+    const auto gateId = nodeId - 1;
+    assert(gateId < triqCirc.gates.size());
+    auto neighborNodeIds = graphView->getNeighborList(nodeId);
+    for (const auto& neighborNodeId : neighborNodeIds)
+    {
+      if (neighborNodeId < graphView->order() - 1)
+      {
+        auto nextNode = graphView->getVertexProperties(neighborNodeId);
+        const auto nextGateId = nextNode.get<std::size_t>("id") - 1;
+        assert(nextGateId < triqCirc.gates.size());
+        assert(gateId != nextGateId);
+        // Add DAG connection information
+        triqCirc.gates[nextGateId]->pred.emplace_back(triqCirc.gates[gateId]);
+        triqCirc.gates[gateId]->succ.emplace_back(triqCirc.gates[nextGateId]);
+      }
+    }
   }
   // DEBUG:
   triqCirc.print_gates();
