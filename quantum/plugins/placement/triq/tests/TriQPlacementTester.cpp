edb5181959856c23b7f7dd227bc74e5c4d01809a
Thien Nguyen <nguyentm@ornl.gov>
2020-08-25 14:53:52 -0400

    Work on constructing TriQ circuit from XACC IR
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- quantum/plugins/placement/triq/tests/TriQPlacementTester.cpp
+++ quantum/plugins/placement/triq/tests/TriQPlacementTester.cpp
@@ -6,7 +6,20 @@
 
 using namespace xacc;
 
-TEST(TriQPlacementTester, checkSimple) {}
+TEST(TriQPlacementTester, checkSimple) {
+  auto irt = xacc::getIRTransformation("triQ");
+  auto compiler = xacc::getCompiler("xasm");
+  auto program = compiler
+                     ->compile(R"(__qpu__ void test(qreg q) {
+      T(q[0]);
+      T(q[0]);
+      CNOT(q[0], q[1]);
+      X(q[1]);
+  })")
+                     ->getComposite("test");
+
+  irt->apply(program, nullptr);
+}
 
 int main(int argc, char **argv) {
   xacc::Initialize(argc, argv);
