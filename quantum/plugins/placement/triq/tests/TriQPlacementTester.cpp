fea60f14c0998323137cad737564c0a7b53388a7
Thien Nguyen <nguyentm@ornl.gov>
2020-09-03 14:34:00 -0400

    Work around an edge case of zero gate fidelity
    Today, ibmq_16_melbourne has Q11 effectively isolated, CNOT gates with neighbors have gate error = 1.0.
    
    This causes TriQ to choke and eventually crash.
    
    We just put in a hack/work-around for now by assigning a rather small fidelity (relatively).
    
    The crash can be prevented. However, the placement runtime for this scenario is still very high.
    
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- quantum/plugins/placement/triq/tests/TriQPlacementTester.cpp
+++ quantum/plugins/placement/triq/tests/TriQPlacementTester.cpp
@@ -98,8 +98,71 @@ TEST(TriQPlacementTester, checkSimple) {
   EXPECT_GT(buffer->computeMeasurementProbability("111"), 0.35);
 }
 
+// Note: this test took a long time to complete.
+// Hence, we don't enable it by default.
+// Test the edge case whereby one qubit becomes effectively
+// isolated from all others.
+// i.e. the CNOT gate fidelity b/w its and neighbors is zero fidelity.
+// TEST(TriQPlacementTester, checkFailedQubit) {
+//   // Allocate some qubits
+//   auto buffer = xacc::qalloc(12);
+//   auto xasmCompiler = xacc::getCompiler("xasm");
+//   auto ir = xasmCompiler->compile(R"(__qpu__ void ghz(qbit q) {
+//       H(q[0]);
+//       H(q[1]);
+//       H(q[2]);
+//       H(q[3]);
+//       H(q[4]);
+//       H(q[5]);
+//       H(q[6]);
+//       H(q[7]);
+//       H(q[8]);
+//       CX(q[8], q[9]);
+//       CX(q[9], q[10]);
+//       CX(q[10], q[11]);
+//       Measure(q[0]);
+//       Measure(q[1]);
+//       Measure(q[2]);
+//       Measure(q[3]);
+//       Measure(q[4]);
+//       Measure(q[5]);
+//       Measure(q[6]);
+//       Measure(q[7]);
+//       Measure(q[8]);
+//       Measure(q[9]);
+//       Measure(q[10]);
+//       Measure(q[11]);
+// })",
+//                                   nullptr);
+//   auto program = ir->getComposites()[0];
+//   auto irt = xacc::getIRTransformation("triQ");
+//   const std::string BACKEND_JSON_FILE =
+//       std::string(RESOURCE_DIR) + "/backend_bit11.json";
+//   std::ifstream inFile;
+//   inFile.open(BACKEND_JSON_FILE);
+//   std::stringstream strStream;
+//   strStream << inFile.rdbuf();
+//   const std::string jsonStr = strStream.str();
+//   irt->apply(program, nullptr, {{"backend-json", jsonStr}});
+//   std::cout << "HOWDY: \n" << program->toString() << "\n";
+//   for (size_t instIdx = 0; instIdx < program->nInstructions(); ++instIdx) {
+//     auto instPtr = program->getInstruction(instIdx);
+//     // Check routing:
+//     if (instPtr->bits().size() == 2) {
+//       // In this particular JSON, Q11 of the the Melbourne backend is
+//       // effectively isolated, the CNOT gates w/ Q11 have error rates of 1.0. We
+//       // check that TriQ can avoid that qubit.
+//       const size_t q0 = instPtr->bits()[0];
+//       const size_t q1 = instPtr->bits()[1];
+//       EXPECT_NE(q0, 11);
+//       EXPECT_NE(q0, 11);
+//     }
+//   }
+// }
+
 int main(int argc, char **argv) {
   xacc::Initialize(argc, argv);
+  xacc::set_verbose(true);
   ::testing::InitGoogleTest(&argc, argv);
   auto ret = RUN_ALL_TESTS();
   xacc::Finalize();
