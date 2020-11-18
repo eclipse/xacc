ef92ec77e857fa3fb4f30a55f0c7c41c6fed46ac
Thien Nguyen <nguyentm@ornl.gov>
2020-11-18 16:13:36 -0500

    QLM Batch Job submission
    Tested with VQE.
    
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- quantum/plugins/atos_qlm/accelerator/tests/QlmAcceleratorTester.cpp
+++ quantum/plugins/atos_qlm/accelerator/tests/QlmAcceleratorTester.cpp
@@ -173,6 +173,42 @@ TEST(QlmAcceleratorTester, testFsim) {
   }
 }
 
+// Batch mode
+TEST(QsimAcceleratorTester, testVqe) {
+  auto accelerator = xacc::getAccelerator("atos-qlm");
+  // Create the N=2 deuteron Hamiltonian
+  auto H_N_2 = xacc::quantum::getObservable(
+      "pauli", std::string("5.907 - 2.1433 X0X1 "
+                           "- 2.1433 Y0Y1"
+                           "+ .21829 Z0 - 6.125 Z1"));
+
+  auto optimizer = xacc::getOptimizer("nlopt");
+  xacc::qasm(R"(
+        .compiler xasm
+        .circuit deuteron_ansatz
+        .parameters theta
+        .qbit q
+        X(q[0]);
+        Ry(q[1], theta);
+        CNOT(q[1],q[0]);
+    )");
+  auto ansatz = xacc::getCompiled("deuteron_ansatz");
+
+  // Get the VQE Algorithm and initialize it
+  auto vqe = xacc::getAlgorithm("vqe");
+  vqe->initialize({std::make_pair("ansatz", ansatz),
+                   std::make_pair("observable", H_N_2),
+                   std::make_pair("accelerator", accelerator),
+                   std::make_pair("optimizer", optimizer)});
+
+  // Allocate some qubits and execute
+  auto buffer = xacc::qalloc(2);
+  vqe->execute(buffer);
+  std::cout << "Energy: " << (*buffer)["opt-val"].as<double>() << "\n";
+  // Expected result: -1.74886
+  EXPECT_NEAR((*buffer)["opt-val"].as<double>(), -1.74886, 1e-3);
+}
+
 int main(int argc, char **argv) {
   xacc::Initialize();
   ::testing::InitGoogleTest(&argc, argv);
