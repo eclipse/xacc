19df08b250502606e54c5d33d4995fdb04a3373a
Alex McCaskey <mccaskeyaj@ornl.gov>
2021-01-14 10:43:09 -0500

    adding vector <pair<int,int>> to py het map
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- python/py_heterogeneous_map.hpp
+++ python/py_heterogeneous_map.hpp
@@ -56,7 +56,7 @@ template <> struct visit_helper<mpark::variant> {
 using PyHeterogeneousMapTypes =
     xacc::Variant<bool, int, double, std::string, std::vector<std::string>,
                   std::vector<double>, std::vector<int>, std::complex<double>, std::vector<std::complex<double>>,
-                  std::shared_ptr<CompositeInstruction>,
+                  std::shared_ptr<CompositeInstruction>, std::vector<std::pair<int,int>>,
                   std::shared_ptr<Instruction>, std::shared_ptr<Accelerator>,
                   std::shared_ptr<Observable>, std::shared_ptr<Optimizer>, Eigen::MatrixXcd>;
 using PyHeterogeneousMap = std::map<std::string, PyHeterogeneousMapTypes>;
