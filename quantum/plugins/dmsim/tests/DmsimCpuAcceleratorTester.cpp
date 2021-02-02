ea760a4cbf9db8c30799899acbe6807b4c6a20e5
A.M. Santana <santanaam@ornl.gov>
2021-02-02 12:18:10 -0500

    Updating full dmsim visitor class
    Signed-off-by: A.M. Santana <santanaam@ornl.gov>

--- quantum/plugins/dmsim/tests/DmsimCpuAcceleratorTester.cpp
+++ quantum/plugins/dmsim/tests/DmsimCpuAcceleratorTester.cpp
@@ -29,6 +29,21 @@ TEST(DmsimcpuAcceleratorTester, testBasic)
     auto ir = xasmCompiler->compile(R"(__qpu__ void ansatz(qbit q, double t) {
       H(q[0]);
       CX(q[0], q[1]);
+      CY(q[0], q[1]);
+      CZ(q[1], q[0]);
+      X(q[0]);
+      Y(q[0]);
+      Z(q[1]);
+      Rz(q[0], 0.25);
+      Rx(q[1], 0.25);
+      Ry(q[0], 0.25);
+      Swap(q[1], q[0]);
+      CRZ(q[0], q[1], 0.25);
+      CH(q[0],q[1]);
+      S(q[0]);
+      T(q[1]);
+      Tdg(q[0]);
+      U(q[1], 0.05, 0.15, 0.25);
     })", accelerator)->getComposites()[0];
     auto buffer = xacc::qalloc(2);
     accelerator->execute(buffer, ir);
