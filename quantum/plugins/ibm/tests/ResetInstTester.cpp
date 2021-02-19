95a80b015206efbcc3b7ca2d97125fcf380bf862
Thien Nguyen <nguyentm@ornl.gov>
2021-02-19 12:33:35 -0500

    Added compile test for XASM and Qobj run
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- quantum/plugins/ibm/tests/ResetInstTester.cpp
+++ quantum/plugins/ibm/tests/ResetInstTester.cpp
@@ -2,8 +2,8 @@
 #include <gtest/gtest.h>
 #include "xacc.hpp"
 
-TEST(ResetInstTester,checkResetCompile) {
-auto src = R"#(
+TEST(ResetInstTester, checkResetCompileStaq) {
+  auto src = R"#(
 OPENQASM 2.0;
 include "qelib1.inc";
 qreg q[1];
@@ -17,8 +17,49 @@ measure q[0] -> c[0];)#";
   auto IR = compiler->compile(src);
   auto hello = IR->getComposites()[0];
   std::cout << "HELLO:\n" << hello->toString() << "\n";
+  EXPECT_EQ(hello->nInstructions(), 5);
+  EXPECT_EQ(hello->getInstruction(1)->name(), "Reset");
+  EXPECT_EQ(hello->getInstruction(2)->name(), "Reset");
 }
 
+TEST(ResetInstTester, checkResetCompileXasm) {
+  auto src = R"#(__qpu__ void reset_test(qbit q) {
+  H(q[0]);
+  Reset(q[0]);
+  Reset(q[0]);
+  X(q[0]);
+  Measure(q[0]);
+})#";
+  auto compiler = xacc::getCompiler("xasm");
+  auto IR = compiler->compile(src);
+  auto hello = IR->getComposites()[0];
+  std::cout << "HELLO:\n" << hello->toString() << "\n";
+  EXPECT_EQ(hello->nInstructions(), 5);
+  EXPECT_EQ(hello->getInstruction(1)->name(), "Reset");
+  EXPECT_EQ(hello->getInstruction(2)->name(), "Reset");
+}
+
+TEST(ResetInstTester, checkQobjRun) {
+  auto acc = xacc::getAccelerator("ibm:ibmq_manhattan");
+  auto src = R"#(__qpu__ void reset_run(qbit q) {
+  H(q[0]);
+  Reset(q[0]);
+  Reset(q[0]);
+  X(q[0]);
+  Measure(q[0]);
+})#";
+  auto compiler = xacc::getCompiler("xasm");
+  auto IR = compiler->compile(src);
+  auto hello = IR->getComposites()[0];
+  std::cout << "HELLO:\n" << hello->toString() << "\n";
+  xacc::set_verbose(true);
+  auto buffer = xacc::qalloc(1);
+  acc->execute(buffer, hello);
+  xacc::set_verbose(false);
+  buffer->print();
+  // Due to reset => back to 0 => become 1 after X...
+  EXPECT_TRUE(buffer->computeMeasurementProbability("1") > 0.8);
+}
 
 int main(int argc, char **argv) {
   xacc::Initialize(argc, argv);
