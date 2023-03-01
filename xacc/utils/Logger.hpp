e1bb8a399ca72f70490a0e23b65c6d127ce1e744
Thien Nguyen <thien.md.nguyen@gmail.com>
2023-03-01 05:06:35 +0000

    Update spdlog tpls

---
+++ xacc/utils/Logger.hpp
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2017 UT-Battelle, LLC.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompanies this
+ * distribution. The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html and the Eclipse Distribution
+ *License is available at https://eclipse.org/org/documents/edl-v10.php
+ *
+ * Contributors:
+ *   Thien Nguyen - initial API and implementation
+ *******************************************************************************/
+
+#pragma once
+
+#include "spdlog/sinks/stdout_sinks-inl.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/spdlog.h"
