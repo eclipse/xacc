129d2ffac85a071fb5f4f87dcb0ef16b1fe94bf0
Daniel Claudino <6d3@ornl.gov>
2023-09-01 17:55:57 +0000

    Azure plugin test skips if plugin is not installed
    Signed-off-by: Daniel Claudino <6d3@ornl.gov>

--- python/tests/azure_testing.py
+++ python/tests/azure_testing.py
@@ -1,9 +1,24 @@
 import unittest as test
 import xacc
 
+def check_plugin_install():
+  try:
+      f = open("../../build/CMakeCache.txt", "r").read()
+  except FileNotFoundError:
+      print("Could not find CMakeCache.txt in the build directory. Make sure you build XACC before running tests.")
+      return True
 
+  if 'XACC_AZURE_PLUGIN' not in f:
+      return True
+  else:
+      line = [l for l in f.splitlines() if "XACC_AZURE_PLUGIN"  in l][0]
+      if "OFF" in line:
+          return True
+      else:
+          return False
+
+@test.skipIf(check_plugin_install(), "Azure plugin not installed.")
 class TestAzurePlugin(test.TestCase):
-
 
     def test_install(self):
       try:
@@ -12,20 +27,16 @@ class TestAzurePlugin(test.TestCase):
         if 'Invalid Accelerator' in e.__str__():
           xacc.error("Azure plugin not installed. Build XACC from source with the -DXACC")   
 
-
     def test_initialize(self):
       qpu = xacc.getAccelerator('azure')
-
 
     def test_name(self):
       qpu = xacc.getAccelerator('azure')
       self.assertEqual(qpu.name(), "azure")
 
-
     def test_update_configuration(self):
       qpu = xacc.getAccelerator('azure')
       qpu.updateConfiguration({"backend": "microsoft.estimator"})
-
 
     def test_execute(self):
       xacc.qasm('''.compiler xasm
@@ -40,7 +51,6 @@ class TestAzurePlugin(test.TestCase):
       buffer = xacc.qalloc(2)
       qpu = xacc.getAccelerator('azure', {"shots": 1024, "backend": "microsoft.estimator",  "visitor": "pyqir"})
       qpu.execute(buffer, ansatz)
-
 
 if __name__ == '__main__':
     test.main()
